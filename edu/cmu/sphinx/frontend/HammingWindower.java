6d3046f9d371ff9c2ca8ad349bc79e5af315cff7
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2002-03-14 20:36:55 +0000

    Refactored the HammingWindow, parameterized the alpha value.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@300 94700074-3cef-4d97-a70e-9c8c206c02f5

--- edu/cmu/sphinx/frontend/HammingWindower.java
+++ edu/cmu/sphinx/frontend/HammingWindower.java
@@ -10,12 +10,27 @@ import edu.cmu.sphinx.util.SphinxProperties;
 /**
  * Applies a HammingWindow to the given DoubleAudioFrame. The audio samples
  * are modified in place, and the original DoubleAudioFrame is returned.
+ * The HammingWindow, <i>W</i> of length <i>N</i> (usually the window size)
+ * is given by the following:
+ * <pre>
+ * W(n) = (1-a) - (a * cos((2*PI*n)/(N - 1))) </pre> where:
+ * <br><b>a</b> is commonly known as the "alpha" value
+ * <br><b>PI</b> is 3.14159265358979323846
  */
 public class HammingWindower implements Processor {
+
+    /**
+     * The name of the SphinxProperty for the alpha value of the Hamming
+     * Window.
+     */
+    public static final String PROP_ALPHA =
+	"edu.cmu.sphinx.frontend.hammingWindow.alpha";
 
     private double[] window;
     private int windowSize;
     private static final double PI = 3.14159265358979323846;
+    private static double ALPHA;
+    private static double ONE_MINUS_ALPHA;
 
 
     /**
@@ -34,6 +49,8 @@ public class HammingWindower implements Processor {
 	// TODO : specify the context
 	SphinxProperties properties = SphinxProperties.getSphinxProperties("");
 	windowSize = properties.getInt(FrontEnd.PROP_WINDOW_SIZE, 205);
+	ALPHA = properties.getDouble(HammingWindower.PROP_ALPHA, 0.46);
+	ONE_MINUS_ALPHA = (1 - ALPHA);
     }
 
 
@@ -44,7 +61,7 @@ public class HammingWindower implements Processor {
 	this.window = new double[windowSize];
 	if (windowSize > 1){
 	    for (int i = 0; i < windowSize; i++) {
-		window[i] = 0.54 - 0.46 *
+		window[i] = ONE_MINUS_ALPHA - ALPHA *
 		    Math.cos(2 * PI * i / ((double) windowSize - 1.0));
 	    }
 	}
