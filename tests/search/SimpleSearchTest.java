a3e9500c41e295529caba8f4a592b5d9226bb8bf
lamere <lamere@94700074-3cef-4d97-a70e-9c8c206c02f5>
2002-05-21 18:27:04 +0000

    Added SimpleSearchTest that runs the search against the SimpleLinguist
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@445 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ tests/search/SimpleSearchTest.java
@@ -0,0 +1,212 @@
+
+/**
+ * [[[copyright]]]
+ */
+package tests.search;
+
+import edu.cmu.sphinx.model.acoustic.AcousticModel;
+
+import edu.cmu.sphinx.search.Dictionary;
+import edu.cmu.sphinx.search.Linguist;
+import edu.cmu.sphinx.search.SimpleLinguist;
+import edu.cmu.sphinx.search.SearchManager;
+import edu.cmu.sphinx.search.BreadthFirstSearchManager;
+import edu.cmu.sphinx.search.AcousticScorer;
+import edu.cmu.sphinx.search.Pruner;
+import edu.cmu.sphinx.search.Token;
+import edu.cmu.sphinx.search.Result;
+
+import edu.cmu.sphinx.util.SphinxProperties;
+
+import java.io.File;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Iterator;
+
+
+/**
+ * A simple search test
+ */
+public class SimpleSearchTest {
+
+
+    private String context = "SimpleSearchTest";
+    private SimpleLinguist simpleLinguist;
+    private SearchManager searchManager;
+    private AcousticScorer acousticScorer;
+    private Pruner pruner;
+
+
+    /**
+     * Construct a SimpleLinguistTest with the given SphinxProperties file.
+     *
+     * @param propertiesFile a SphinxProperties file
+     */
+    public SimpleSearchTest(String propertiesFile) throws Exception {
+        
+        String pwd = System.getProperty("user.dir");
+        SphinxProperties.initContext
+            (context, new URL
+             ("file://" + pwd + File.separatorChar + propertiesFile));
+        
+        simpleLinguist = new SimpleLinguist
+            (context, AcousticModel.getAcousticModel(context));
+
+	acousticScorer = new SimpleAcousticScorer();
+	pruner = new SimplePruner();
+
+	searchManager = new BreadthFirstSearchManager(
+		simpleLinguist, acousticScorer, pruner);
+	dumpGrammar();
+	dumpSentenceHMM();
+    }
+
+
+    /**
+     * Dumps the SentenceHMM
+     */
+    public void dumpSentenceHMM() {
+	simpleLinguist.getInitialState().dump();
+    }
+
+
+    /**
+     * Dumps the grammar 
+     */
+    public void dumpGrammar() {
+        System.out.print
+            (((SimpleLinguist) simpleLinguist).getGrammar().toString());
+    }
+
+
+    /**
+     * Perform a search
+     */
+    public void search() {
+	searchManager.initialize();
+	Result result = null;
+
+	for (int i = 0; i < 100; i++) {
+	    result = searchManager.recognize(1);
+	    showActive(result);
+	    showResultSummary(result);
+	    // showResult(result);
+	}
+	searchManager.terminate();
+    }
+
+
+
+    /**
+     * Shows the result
+     */
+
+    public void showResult(Result result) {
+	System.out.println(" -------------- results ---------------- ");
+	for (Iterator i = result.getResultTokens().iterator(); i.hasNext(); ) {
+	    Token token = (Token) i.next();
+	    assert(token.isFinal());
+	    System.out.println( "score: " + token.getScore() + 
+			" path: " + token.getWordPath() +
+			(token.isFinal() ? " final" : "") );
+	    token.dumpTokenPath();
+	}
+    }
+
+    /**
+     * Shows the result
+     */
+
+    public void showResultSummary(Result result) {
+	System.out.println(" -------------- results ---------------- ");
+	for (Iterator i = result.getResultTokens().iterator(); i.hasNext(); ) {
+	    Token token = (Token) i.next();
+	    assert(token.isFinal());
+	    System.out.println( "score: " + token.getScore() + 
+			" path: " + token.getWordPath() +
+			(token.isFinal() ? " final" : "") );
+	}
+    }
+
+    public void showActive(Result result) {
+	System.out.println(" -------------- active  ---------------- ");
+	for (Iterator i = result.getActiveTokens().iterator(); i.hasNext(); ) {
+	    Token token = (Token) i.next();
+	    System.out.println( "score: " + token.getScore() + 
+			" path: " + token.getWordPath() +
+			(token.isFinal() ? " final" : "") );
+	    token.dumpTokenPath();
+	}
+    }
+
+    /**
+     * Main method of this Test.
+     */
+    public static void main(String[] argv) {
+        try {
+            SimpleSearchTest test = new SimpleSearchTest(argv[0]);
+	    test.search();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+/**
+ * A simple pruner that doesn't really prune
+ */
+class SimplePruner implements Pruner {
+    /**
+     * Initializes the scorer
+     */
+    public void initialize() {
+    }
+
+    /**
+     * prunes the given set of states
+     *
+     * @param stateTokenList a list containing StateToken objects to
+     * be scored
+     */
+    public void prune(List stateTokenList) {
+	System.out.println("Pruner: " + stateTokenList.size() 
+		+ " tokens in activeList");
+    }
+
+
+    /**
+     * Performs post-recognition cleanup. 
+     */
+    public void terminate() {
+    }
+}
+
+
+class SimpleAcousticScorer implements AcousticScorer {
+    /**
+     * Initializes the scorer
+     */
+    public void initialize() {
+    }
+
+    /**
+     * Scores the given set of states
+     *
+     * @param stateTokenList a list containing StateToken objects to
+     * be scored
+     */
+    public void calculateScores(List stateTokenList) {
+	for (Iterator i = stateTokenList.iterator(); i.hasNext(); ) {
+	    Token token = (Token) i.next();
+	    assert(token.isEmitting());
+	}
+    }
+
+    /**
+     * Performs post-recognition cleanup. 
+     */
+    public void terminate() {
+    }
+}
