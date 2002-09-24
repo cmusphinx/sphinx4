828067bbe123ae71bf573eda28ae3c7a07efdb7d
lamere <lamere@94700074-3cef-4d97-a70e-9c8c206c02f5>
2002-09-24 10:32:08 +0000

    Added tests (with associated grammar, props and dictionaries) for the ArpaGrammar loader
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@544 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ tests/search/ArpaGrammarTest.java
@@ -0,0 +1,41 @@
+/**
+ * [[[copyright]]]
+ */
+package tests.search;
+
+import edu.cmu.sphinx.util.SphinxProperties;
+
+import edu.cmu.sphinx.search.Dictionary;
+import edu.cmu.sphinx.search.FullDictionary;
+import edu.cmu.sphinx.search.Grammar;
+import edu.cmu.sphinx.search.ArpaGrammar;
+
+import java.io.File;
+
+import java.net.URL;
+
+
+/**
+ * A test for the ArpaGrammar class 
+ */
+public class ArpaGrammarTest {
+    private static String context = "ArpaGrammar";
+
+    /**
+     * Main method of this Test.
+     */
+    public static void main(String[] argv) {
+        try {
+	    String pwd = System.getProperty("user.dir");
+	    SphinxProperties.initContext (context, new URL ("file://" 
+			       + pwd + File.separatorChar + argv[0])); 
+	    Dictionary dictionary = new FullDictionary(context);
+            Grammar grammar = new ArpaGrammar();
+
+	    grammar.initialize(context, dictionary);
+	    grammar.dump();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
