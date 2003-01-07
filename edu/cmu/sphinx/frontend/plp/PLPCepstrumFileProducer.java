a00ed21c71aeab0ae908dc34585f42fe600a4554
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-01-07 16:25:58 +0000

    Adding all the PLP related files into the plp directory.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@944 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ edu/cmu/sphinx/frontend/plp/PLPCepstrumFileProducer.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+
+package edu.cmu.sphinx.frontend.plp;
+
+import edu.cmu.sphinx.frontend.Cepstrum;
+import edu.cmu.sphinx.frontend.CepstrumSource;
+import edu.cmu.sphinx.frontend.DataProcessor;
+import edu.cmu.sphinx.frontend.FrontEnd;
+import edu.cmu.sphinx.frontend.Signal;
+
+import edu.cmu.sphinx.util.SphinxProperties;
+import edu.cmu.sphinx.util.ExtendedStreamTokenizer;
+
+import java.io.IOException;
+
+
+/**
+ * Produces PLPCepstrum data from a file.
+ */
+public class PLPCepstrumFileProducer extends DataProcessor implements
+CepstrumSource {
+
+    private final static String PROP_CEPSTRUM_FILE =
+    FrontEnd.PROP_PREFIX + "PLPCepstrumFileProducer.file";
+
+
+    private String path;
+    private ExtendedStreamTokenizer est;
+    private int numFrames;
+    private int curFrame;
+    private int cepstrumLength;
+
+
+    /**
+     * Constructs a PLPCepstrumFileProducer that reads
+     * PLPCepstrum data from the given path.
+     *
+     * @param context the context for the producer
+     *
+     * @throws IOException if an error occurs while reading the data
+     */
+    public PLPCepstrumFileProducer(String name, String context) throws
+    IOException {
+	super(name, context);
+	initSphinxProperties();
+	est = new ExtendedStreamTokenizer(path);
+	numFrames = est.getInt("num_frames");
+	est.expectString("frames");
+    }
+
+
+    /**
+     * Reads the parameters needed from the static SphinxProperties object.
+     */
+    private void initSphinxProperties() {
+	SphinxProperties properties = getSphinxProperties();
+	cepstrumLength = properties.getInt(FrontEnd.PROP_CEPSTRUM_SIZE, 13);
+	path = properties.getString(PROP_CEPSTRUM_FILE, "file");
+	System.out.println("File is " + path);
+    }
+
+
+    /**
+     * Returns the next Cepstrum object, which is the PLP cepstrum of the
+     * input frame. However, it can also be other Cepstrum objects
+     * like a EndPointSignal.
+     *
+     * @return the next available Cepstrum object, returns null if no
+     *     Cepstrum object is available
+     */
+    public Cepstrum getCepstrum() throws IOException {
+
+	Cepstrum data = null;
+
+	if (curFrame == 0) {
+	    data = new Cepstrum(Signal.UTTERANCE_START);
+	} else if (curFrame == numFrames) {
+            data = new Cepstrum(Signal.UTTERANCE_END);
+	} else if (curFrame > numFrames) {
+            data = null;
+	} else {
+	    float[] vectorCepstrum = new float[cepstrumLength];
+	    for (int i = 0; i < cepstrumLength; i++) {
+		vectorCepstrum[i] = est.getFloat("cepstrum data");
+	    }
+	    data  = new Cepstrum(vectorCepstrum);
+	    System.out.println("CP " + data);
+	}
+	curFrame++;
+	return data;
+    }
+}
---
+++ edu/cmu/sphinx/frontend/plp/PLPFilter.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+
+package edu.cmu.sphinx.frontend.plp;
+
+import edu.cmu.sphinx.util.SphinxProperties;
+
+import java.io.IOException;
+
+/**
+ * Define a filter used by the PLPFilterbank class.  PLPFilterbank
+ * uses this class to create filters and operate on them.  
+ *
+ * Created: Tue Dec 24 17:56:39 2002
+ *
+ * @author <a href="mailto:rsingh@cs.cmu.edu">rsingh</a>
+ * @version 1.0
+ */
+public class PLPFilter {
+    private double[] filterCoefficients;
+    private int numDFTPoints;
+
+    /**
+     * <code>centerFreqInHertz</code> specifies the center frequency of
+     * the filter in Hertz;
+     */
+    public double centerFreqInHz;
+
+    /**
+     * <code>centerFreqInBark</code> specifies the center frequency of
+     * the filter in Bark;
+     */
+    public double centerFreqInBark;
+
+    /**
+     * Constructs a PLP filter around a given center frequency.<br>
+     * Defines a filter according to the following equation 
+     * (all frequencies in the equation are Bark frequencies):<br>
+     * Filter(f) = 0 if f < -2.5 <br>
+     *           = 10^(-(f+0.5)) if -2.5 <= f <= -0.5 <br>
+     *           = 1  if -0.5 <= f <= 0.5 <br>
+     *           = 10^(2.5(f-0.5)) if 0.5 <= f <= 1.3 <br>
+     *           = 0 if f > 1.3 <br>
+     *
+     * The current implementation assumes that the calling routine passes in
+     * an array of frequencies, one for each of the DFT points in the
+     * spectrum of the frame of speech to be filtered. This is used in
+     * conjunction with a specified center frequency to determine the filter.
+     *
+     * @param DFTFrequenciesInHz is a double array containing the frequencies
+     *        in Hertz corresponding to each of the DFT points in the spectrum
+     *        of the signal to be filtered.
+     * @param centerFreq is the filter's center frequency
+     *
+     * @throws IllegalArgumentException
+     *
+     */
+
+
+    public PLPFilter(double[] DFTFrequenciesInHz, 
+		     double centerFreqInHz) throws IllegalArgumentException {
+
+	FrequencyWarper bark = new FrequencyWarper();
+
+	numDFTPoints = DFTFrequenciesInHz.length;
+	this.centerFreqInHz = centerFreqInHz;
+	centerFreqInBark = bark.hertzToBark(centerFreqInHz);
+	
+	if (centerFreqInHz < DFTFrequenciesInHz[0] || 
+	    centerFreqInHz > DFTFrequenciesInHz[numDFTPoints-1]) {
+	    throw new IllegalArgumentException("Center frequency for PLP filter out of range");
+	}
+
+	filterCoefficients = new double[numDFTPoints];
+
+	for (int i=0; i<numDFTPoints; i++){
+	    double barkf;
+
+	    barkf = bark.hertzToBark(DFTFrequenciesInHz[i]) - centerFreqInBark;
+	    if (barkf < -2.5)
+		filterCoefficients[i] = 0.0;
+	    else if (barkf <= -0.5)
+		filterCoefficients[i] = Math.pow(10.0, -(barkf+0.5));
+	    else if (barkf <= 0.5)
+		filterCoefficients[i] = 1.0;
+	    else if (barkf <= 1.3)
+		filterCoefficients[i] = Math.pow(10.0, 2.5*(barkf-0.5));
+	    else
+		filterCoefficients[i] = 0.0;
+	}
+    }
+
+    /**
+     * Compute the PLP spectrum at the center frequency of this filter
+     * for a given power spectrum.
+     *
+     * @param spectrum the input power spectrum to be filtered
+     *
+     * @return the PLP spectrum value
+     *
+     * @throws IllegalArgumentException if the input spectrum is of a
+     *         different length than the array of filter coefficients
+     */
+    public double filterOutput(double[] spectrum)
+	throws IllegalArgumentException {
+
+	if (spectrum.length != numDFTPoints){
+	    throw new IllegalArgumentException("Mismatch in no. of DFT points in spectrum and filter");
+	}
+
+	double output = 0.0;
+	for (int i = 0; i < numDFTPoints; i++) {
+	    output += spectrum[i]*filterCoefficients[i];
+	}
+	return output;
+    }
+}
