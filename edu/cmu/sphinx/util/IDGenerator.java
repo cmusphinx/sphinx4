e09e9969b78cfaf3cce2fdbcafab02d9ccf49562
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-01-08 14:19:57 +0000

    Moving this class out of the frontend to the util directory.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@954 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ edu/cmu/sphinx/util/IDGenerator.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+
+package edu.cmu.sphinx.util;
+
+/**
+ * An ID generator that gives out positive integer IDs and
+ * checks for overflow.
+ */ 
+public class IDGenerator {
+
+    /**
+     * Indicates a non-ID value, which is -1.
+     */
+    public static int NON_ID = -1;
+
+    private int id = 0;
+    
+    /**
+     * Returns the next valid ID, checks for integer overflow.
+     *
+     * @return the next valid ID, goes back to zero if overflow
+     */
+    public int getNextID() {
+        if (id == Integer.MAX_VALUE) {
+            return (id = 0);
+        } else {
+            return id++;
+        }
+    }
+
+    /**
+     * Resets the ID to zero.
+     */
+    public void reset() {
+        id = 0;
+    }
+}
+
+
