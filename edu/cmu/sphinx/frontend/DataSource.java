a914137145dfeb8f5583f17c85c54e413ad5c3cd
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-01-10 21:49:43 +0000

    This is the parent interface of all XXXSource interfaces.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@968 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ edu/cmu/sphinx/frontend/DataSource.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+
+package edu.cmu.sphinx.frontend;
+
+/**
+ * A DataSource is the parent interface of all Sources.
+ */
+public interface DataSource {
+}
