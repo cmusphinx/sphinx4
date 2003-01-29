61593a5886f2994147baf27be5d95b57ece62456
lamere <lamere@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-01-29 14:01:31 +0000

    Added linguist dumper interface and a GDL dumper that dumps a sentence hmm for aisee
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@1021 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ edu/cmu/sphinx/util/GDLDumper.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright 1999-2003 Carnegie Mellon University.  
+ * Portions Copyright 2003 Sun Microsystems, Inc.  
+ * Portions Copyright 2003 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+package edu.cmu.sphinx.util;
+
+import edu.cmu.sphinx.search.SentenceHMMState;
+import edu.cmu.sphinx.search.SentenceHMMStateArc;
+import edu.cmu.sphinx.search.LinguistProcessor;
+import edu.cmu.sphinx.search.Linguist;
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import edu.cmu.sphinx.search.WordState;
+import edu.cmu.sphinx.search.UnitState;
+import edu.cmu.sphinx.search.GrammarState;
+import edu.cmu.sphinx.search.HMMStateState;
+import edu.cmu.sphinx.search.AlternativeState;
+import edu.cmu.sphinx.search.PronunciationState;
+
+
+/**
+ *  A linguist processor that dumps out the sentence hmm in GDL
+ *  format.
+ */
+public class GDLDumper extends LinguistDumper  {
+
+    private boolean skipHMMs = true;
+
+
+    /**
+     * Creates a GDL dumper
+     */
+    public GDLDumper() {
+	setDepthFirst(false); // breadth first traversal
+    }
+
+    /**
+     * Retreives the default name for the destination dump. This
+     * method is typically overridden by derived classes
+     *
+     *
+     * @return the default name for the file.
+     */
+    protected String getDefaultName() {
+	return "linguistDump.gdl";
+    }
+
+    /**
+     * Called at the start of the dump
+     *
+     * @param out the output stream.
+     */
+    protected void startDump(PrintStream out) {
+	out.println("graph: {");
+	out.println("    layout_algorithm: minbackward");
+	out.println("    orientation: left_to_right");
+    }
+
+    /**
+     * Called at the end of the dump
+     *
+     * @param out the output stream.
+     */
+    protected void endDump(PrintStream out) {
+	out.println("}");
+    }
+
+    /**
+     * Called to dump out a node in the SentenceHMM
+     *
+     * @param out the output stream.
+     * @param state the state to dump
+     * @param level the level of the state
+     */
+    protected void startDumpNode(PrintStream out, 
+	    SentenceHMMState state, int level) {
+
+	if (skipHMMs && (state instanceof HMMStateState)) {
+	} else {
+	    String color = getColor(state);
+	    String shape = "box";
+
+	    if (state.isFinalState()) {
+		shape = "circle";
+	    }
+	    out.println("    node: {" +
+		    "title: " + qs(state.getSignature()) + 
+		    " label: " + qs(state.getPrettyName()) + 
+		    " color: " + color +
+		    " shape: " + shape + 
+		    " vertical_order: " + level +
+		    "}");
+	}
+    }
+
+
+    /**
+     * Gets the color for a particular state
+     *
+     * @param state the state
+     * 
+     * @return its color
+     */
+
+    private String getColor(SentenceHMMState state) {
+	String color = "black";
+	if (state.isFinalState()) {
+	    color = "magenta";
+	} else if (state instanceof UnitState) {
+	    color = "green";
+	} else if (state instanceof WordState) {
+	    color = "lightblue";
+	} else if (state instanceof GrammarState) {
+	    color = "lightred";
+	} else if (state instanceof HMMStateState) {
+	    color = "orange";
+	} else if (state instanceof AlternativeState) {
+	    color = "purple";
+	} else if (state instanceof PronunciationState) {
+	    color = "darkcyan";
+	}
+	return color;
+    }
+
+    /**
+     * Called to dump out a node in the SentenceHMM
+     *
+     * @param out the output stream.
+     * @param state the state to dump
+     * @param level the level of the state
+     */
+    protected void endDumpNode(PrintStream out, SentenceHMMState state, 
+	    int level) {
+    }
+
+    /**
+     * Dumps an arc
+     *
+     * @param out the output stream.
+     * @param from arc leaves this state
+     * @param arc the arc to dump
+     * @param level the level of the state
+     */
+    protected void dumpArc(PrintStream out, SentenceHMMState from, 
+	    	SentenceHMMStateArc arc, int level) {
+
+	SentenceHMMState nextState = arc.getNextState();
+
+	if (skipHMMs) {
+	    if (nextState instanceof HMMStateState) {
+		return;
+	    } else if (from instanceof HMMStateState) {
+		from = from.getParent();
+	    }
+	} 
+	out.println("   edge: { sourcename: " +
+	    qs(from.getSignature()) + " targetname: " +
+	    qs(nextState.getSignature()) + "}");
+    }
+
+
+    /**
+     * Returns a quoted string version of its argument. This method
+     * mainly is used to hide the ugliness caused by trying to esape a
+     * quote character in certain syntax higlighting editors such as
+     * vim.
+     *
+     * @param s the string to quote.
+     *
+     * @return the quoted string
+     */
+    private String qs(String s) {
+	return "\"" + s + "\"";
+    }
+}
+
