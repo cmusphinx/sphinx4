3cd6cabf852e3d4f154f7a4c72ddf901a1f2a685
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-01-31 20:38:57 +0000

    Adding the test code for ParallelFrontEnd, the ParallelFrontEndTest, and the corresponding properties file.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@1044 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ tests/frontend/ParallelFrontEndTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+
+package tests.frontend;
+
+import edu.cmu.sphinx.frontend.FeatureFrame;
+import edu.cmu.sphinx.frontend.FrontEnd;
+import edu.cmu.sphinx.frontend.parallel.ParallelFrontEnd;
+
+import edu.cmu.sphinx.model.acoustic.AcousticModel;
+
+import edu.cmu.sphinx.util.Timer;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Test program for the FrontEnd.
+ */
+public class ParallelFrontEndTest {
+
+    public static void main(String[] argv) {
+
+	if (argv.length < 3) {
+	    System.out.println
+                ("Usage: java testClass <testName> " +
+                 "<propertiesFile> <audiofilename>");
+	}
+
+        try {
+            String testName = argv[0];
+            String propertiesFile = argv[1];
+            String audioFile = argv[2];
+
+            ProcessorTest fet = new ProcessorTest
+                (testName, propertiesFile, audioFile);
+
+            FrontEnd fe = new ParallelFrontEnd
+		("ParallelFrontEnd", testName, fet.getAudioSource());
+           
+	    System.out.println(fe.toString());
+
+	    List namelist = AcousticModel.getNames(testName);
+
+	    for (Iterator i = namelist.iterator(); i.hasNext();) {
+		String amName = (String) i.next();
+		FeatureFrame featureFrame = null;
+		do {
+		    featureFrame = fe.getFeatureFrame(25, amName);
+		    if (featureFrame != null) {
+			System.out.println(featureFrame.toString());
+		    }
+		} while (featureFrame != null);
+	    }
+
+            if (fet.getDumpTimes()) {
+                Timer.dumpAll();
+            }
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
+}
