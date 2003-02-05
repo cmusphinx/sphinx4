8faa5174ca7bac88e9bd3d85c76e463d3c1a9136
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-02-05 16:50:56 +0000

    Appended the FrontEnd property prefix to property names in Utterance.java.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@1058 94700074-3cef-4d97-a70e-9c8c206c02f5

--- edu/cmu/sphinx/frontend/Utterance.java
+++ edu/cmu/sphinx/frontend/Utterance.java
@@ -53,13 +53,15 @@ public class Utterance {
         SphinxProperties properties = 
             SphinxProperties.getSphinxProperties(context);
 
-        bitsPerSample = properties.getInt(FrontEnd.PROP_BITS_PER_SAMPLE, 16);
-        sampleRate = properties.getInt(FrontEnd.PROP_SAMPLE_RATE, 8000);
+        bitsPerSample = properties.getInt
+	    (FrontEnd.PROP_PREFIX + FrontEnd.PROP_BITS_PER_SAMPLE, 16);
+        sampleRate = properties.getInt
+	    (FrontEnd.PROP_PREFIX + FrontEnd.PROP_SAMPLE_RATE, 16000);
         
         float windowSizeInMs = properties.getFloat
-            (FrontEnd.PROP_WINDOW_SIZE_MS, 25.625F);
+            (FrontEnd.PROP_PREFIX + FrontEnd.PROP_WINDOW_SIZE_MS, 25.625F);
         float windowShiftInMs = properties.getFloat
-            (FrontEnd.PROP_WINDOW_SHIFT_MS, 10.0F);
+            (FrontEnd.PROP_PREFIX + FrontEnd.PROP_WINDOW_SHIFT_MS, 10.0F);
         
         windowSizeInBytes = Util.getSamplesPerWindow
             (sampleRate, windowSizeInMs) * 2;
