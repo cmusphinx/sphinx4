b924cbccdb987fb79b699908fdd497d5c2014b0a
egouvea <egouvea@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-02-27 18:15:09 +0000

    Added trainer interfaces.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@1142 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ edu/cmu/sphinx/trainer/Learner.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+package edu.cmu.sphinx.trainer;
+
+
+import edu.cmu.sphinx.frontend.*;
+import edu.cmu.sphinx.model.acoustic.*;
+
+
+/**
+ * Provides machanisms for computing statistics given a set of states
+ * and input data.
+ */
+public interface  Learner {
+
+    /**
+     * Initializes the Learner with the proper context and frontend.
+     *
+     * @param context the context to use
+     * @param frontend the FrontEnd to use
+     */
+    public void initialize(String context, FrontEnd frontend);
+
+    /**
+     * Starts the Learner.
+     */
+    public void start();
+
+    /**
+     * Stops the Learner.
+     */
+    public void stop();
+
+    /**
+     * Initializes computation for current SentenceHMM.
+     *
+     * @param sentenceHMM sentence HMM being processed
+     */
+    public void initializeComputation(SentenceHMM sentenceHMM);
+
+    /**
+     * Gets posterior probabilities for a given state.
+     *
+     * @param stateID state ID number, relative to the sentence HMM
+     */
+    public Double getScore(int stateID);
+}
