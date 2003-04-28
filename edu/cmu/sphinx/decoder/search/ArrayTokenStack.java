71d7349ca54c9124743b2e642b1f666a7f2ba106
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-04-28 20:21:01 +0000

    Adding TokenStack interface and ArrayTokenStack implementation.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@1337 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ edu/cmu/sphinx/decoder/search/ArrayTokenStack.java
@@ -0,0 +1,125 @@
+
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+package edu.cmu.sphinx.decoder.search;
+
+
+/**
+ * A SentenceHMMState in a parallel branch.
+ */
+public class ArrayTokenStack implements TokenStack {
+
+    private Token[] stack;
+
+
+    /**
+     * Constructs a ArrayTokenStack object with the given capacity.
+     *
+     * @param capacity the height of the stack
+     */
+    public ArrayTokenStack(int capacity) {
+        this.stack = new Token[capacity];
+        clear();
+    }
+
+
+    /**
+     * Adds the given Token to this TokenStack.
+     *
+     * @param newToken the new Token to add
+     *
+     * @return the replaced Token, or null if no Token was replaced
+     */
+    public Token add(Token newToken) {
+        Token lowestToken = null;
+        int lowestTokenIndex = -1;
+
+        // first look for the lowest scoring token
+        for (int i = 0; i < stack.length; i++) {
+            Token token = stack[i];
+	    if (token == null) {
+		stack[i] = newToken;
+		return null;
+	    } else {
+                if (token.getFrameNumber() != newToken.getFrameNumber()) {
+                    stack[i] = newToken;
+                    return token; 
+                }
+                if (lowestToken == null) {
+                    lowestToken = token;
+                    lowestTokenIndex = i;
+                } else if (token.getScore() <= lowestToken.getScore()) {
+                    lowestToken = token;
+                    lowestTokenIndex = i;
+                }
+            }
+        }
+
+        // if found the lowest scoring token
+        if (lowestTokenIndex != -1) {
+            stack[lowestTokenIndex] = newToken;
+            return lowestToken;
+        } else {
+            // if no tokens at all
+            stack[0] = newToken;
+            return null;
+        }
+    }
+            
+
+    /**
+     * Returns true if the given score is higher than the lowest scoring
+     * token in this TokenStack.
+     *
+     * @param score the score the check
+     *
+     * @return true if the given score is higher than the lowest scoring
+     *    token, false otherwise
+     */
+    public boolean isInsertable(float score, int frameNumber) {
+        for (int i = 0; i < stack.length; i++) {
+	    Token token = stack[i];
+            if (token == null || 
+                token.getFrameNumber() != frameNumber ||
+		token.getScore() <= score) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Returns the capacity of this TokenStack, that is, the maximum
+     * number of elements allowed in this TokenStack.
+     *
+     * @return the capacity of this TokenStack
+     */
+    public int getCapacity() {
+        return stack.length;
+    }
+
+
+
+    /**
+     * Removes all of the Tokens from this TokenStack
+     */
+    public void clear() {
+        for (int i = 0; i < stack.length; i++) {
+            stack[i] = null;
+        }
+    }
+}
+
+
+
