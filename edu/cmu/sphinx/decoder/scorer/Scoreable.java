21de2f77c80a647ec306de8660f0dbcf2bf14b08
lamere <lamere@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-05-05 10:42:25 +0000

    A new interface that represents something that can be 'scored'. This is used by the acoustic scorers. Typically, but not necessarily, a token is 'scoreable'.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@1367 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ edu/cmu/sphinx/decoder/scorer/Scoreable.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+package edu.cmu.sphinx.decoder.scorer;
+
+import edu.cmu.sphinx.frontend.Feature;
+
+/**
+ * Represents an entity that can be scored against a feature
+ */
+public interface  Scoreable {
+
+    /**
+     * Calculates a score against the given feature. The score can be
+     * retreived with get score
+     *
+     * @param feature the feature to be scored
+     *
+     * @return the score for the feature
+     */
+    public float calculateScore(Feature feature);
+
+    
+    /**
+     * Retrieves a previously calculated (and possibly normalized) score
+     *
+     * @return the score
+     */
+    public float getScore();
+
+    /**
+     * Normalizes a previously calculated score
+     *
+     * @return the normalized score
+     */
+    public float normalizeScore(float maxScore);
+}
