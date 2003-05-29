c1f2714ac8a2d929f759fbbc31a99f6f954d13a4
lamere <lamere@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-05-29 11:01:32 +0000

    Converted the arrays of text prompt plus index method of managing a sequence of prompts into a full fledged interface and implementation.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@1501 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ demo/jsapi/cardmatch/Prompt.java
@@ -0,0 +1,30 @@
+
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+package demo.jsapi.cardmatch;
+
+
+/**
+ * An interface that represents a spoken prompt
+ */
+public interface Prompt {
+    /**
+     * Gets the next text to be spoken
+     */
+    public String getText();
+
+    /**
+     * Resets the prompt to its initial state
+     */
+    public void reset();
+}
---
+++ demo/jsapi/cardmatch/SequencePrompt.java
@@ -0,0 +1,52 @@
+
+/*
+ * Copyright 1999-2002 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+
+package demo.jsapi.cardmatch;
+
+
+/**
+ * An interface that represents a spoken prompt
+ */
+public class SequencePrompt implements Prompt {
+    private String[] prompts;
+    private int curPrompt = 0;
+
+    /**
+     * Creates a sequence prompt
+     *
+     * @param an array of prompts
+     *
+     */
+    public SequencePrompt(String[] prompts) {
+        this.prompts = prompts;
+    }
+
+    /**
+     * Gets the next text to be spoken
+     */
+    public String getText() {
+        String prompt = prompts[curPrompt++];
+        if (curPrompt >= prompts.length) {
+            curPrompt = 0;
+        }
+        return prompt;
+    }
+
+
+    /**
+     * Resets the prompt to its initial state
+     */
+    public void reset() {
+        curPrompt = 0;
+    }
+}
