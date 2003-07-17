0486b06f9697d2ee8bc92722f58deae61cfc0252
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-07-17 20:28:18 +0000

    ParallelAcousticScorer now checks to see if the currentFrameNumber in the tokens matches the feature ID.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@1721 94700074-3cef-4d97-a70e-9c8c206c02f5

--- edu/cmu/sphinx/research/parallel/ParallelAcousticScorer.java
+++ edu/cmu/sphinx/research/parallel/ParallelAcousticScorer.java
@@ -12,18 +12,20 @@
 
 package edu.cmu.sphinx.research.parallel;
 
+import edu.cmu.sphinx.frontend.FrontEnd;
+import edu.cmu.sphinx.frontend.FeatureFrame;
+import edu.cmu.sphinx.frontend.Feature;
+import edu.cmu.sphinx.frontend.Signal;
+
 import edu.cmu.sphinx.decoder.scorer.AcousticScorer;
+import edu.cmu.sphinx.decoder.scorer.Scoreable;
+
 import edu.cmu.sphinx.decoder.search.Token;
 
 import java.util.Iterator;
 import java.util.List;
 
 import java.io.IOException;
-
-import edu.cmu.sphinx.frontend.FrontEnd;
-import edu.cmu.sphinx.frontend.FeatureFrame;
-import edu.cmu.sphinx.frontend.Feature;
-import edu.cmu.sphinx.frontend.Signal;
 
 
 /**
@@ -36,7 +38,7 @@ public class ParallelAcousticScorer implements AcousticScorer {
 
 
     /**
-     * Initializes this ParallelAcousticScorer with the given
+     * Initializes this SimpleAcousticScorer with the given
      * context and FrontEnd.
      *
      * @param context the context to use
@@ -55,59 +57,95 @@ public class ParallelAcousticScorer implements AcousticScorer {
 
 
     /**
+     * Performs post-recognition cleanup. 
+     */
+    public void stop() {
+    }
+
+
+    /**
+     * Checks to see if a FeatureFrame is null or if there are Features in it.
+     *
+     * @param ff the FeatureFrame to check
+     *
+     * @return false if the given FeatureFrame is null or if there
+     * are no Features in the FeatureFrame; true otherwise.
+     */
+    private boolean hasFeatures(FeatureFrame ff) {
+        if (ff == null) {
+            System.out.println("ParallelAcousticScorer: FeatureFrame is null");
+            return false;
+        }
+        if (ff.getFeatures() == null) {
+            System.out.println
+                ("ParallelAcousticScorer: no features in FeatureFrame");
+            return false;
+        }
+        return true;
+    }
+
+
+
+    /**
      * Scores the given set of Tokens. All Tokens in the given
      * list are assumed to belong to the same acoustic model.
      *
-     * @param stateTokenList a list containing StateToken objects to
+     * @param scoreableList a list containing StateToken objects to
      * be scored
      *
      * @return true if there are more features available
      */
-    public boolean calculateScores(List stateTokenList) {
+    public boolean calculateScores(List scoreableList) {
 
+        assert scoreableList.size() > 0;
+        
+        String modelName = getModelName(scoreableList);
+        if (modelName == null) {
+            System.out.println
+                ("ParallelAcousticScorer: modelName is null");
+        }
+        assert modelName != null;
+        
 	FeatureFrame ff;
 
 	try {
-	    assert stateTokenList.size() > 0;
-
-	    String modelName = getModelName(stateTokenList);
-	    if (modelName == null) {
-		System.out.println
-		    ("ParallelAcousticScorer: modelName is null");
-	    }
-	    assert modelName != null;
-
 	    ff = frontEnd.getFeatureFrame(1, modelName);
-	    Feature feature;            
+	    Feature feature;
 
-	    if (ff == null) {
-		System.out.println("FeatureFrame is null");
-		return false;
-	    }
-
-	    if (ff.getFeatures() == null) {
-		System.out.println("features array is null ");
-		return false;
-	    }
+	    if (!hasFeatures(ff)) {
+                return false;
+            }
 
 	    feature = ff.getFeatures()[0];
 
 	    if (feature.getSignal() == Signal.UTTERANCE_START) {
-		return true;
+                ff = frontEnd.getFeatureFrame(1, modelName);
+                if (!hasFeatures(ff)) {
+                    return false;
+                }
+                feature = ff.getFeatures()[0];
 	    }
+
 	    if (feature.getSignal() == Signal.UTTERANCE_END) {
 		return false;
 	    }
 
-	    for (Iterator i = stateTokenList.iterator(); i.hasNext(); ) {
-		ParallelToken token = (ParallelToken) i.next();
-                if (!token.isPruned()) {
-                    if (feature.hasContent()) {
-                        token.calculateScore(feature);
-                    } else {
-                        System.out.println("non-content feature " +
-                                           feature + " id " + feature.getID());
-                    }
+            if (!feature.hasContent()) {
+                throw new Error("trying to score non-content feature");
+            }
+
+            float logMaxScore = -Float.MAX_VALUE;
+	    for (Iterator i = scoreableList.iterator(); i.hasNext(); ) {
+                Scoreable scoreable = (Scoreable) i.next();
+		if (scoreable.getFrameNumber() != feature.getID()) {
+		    throw new Error
+			("Frame number mismatch: Token: " + 
+			 scoreable.getFrameNumber() +
+			 "  Feature: " + feature.getID());
+		}
+                float logScore =  scoreable.calculateScore(feature);
+                if (logScore > logMaxScore) {
+                    logMaxScore = logScore;
                 }
 	    }
 	} catch (IOException ioe) {
@@ -131,21 +169,9 @@ public class ParallelAcousticScorer implements AcousticScorer {
 	    Iterator i = activeList.iterator();
 	    if (i.hasNext()) {
 		ParallelToken token = (ParallelToken) i.next();
-		/*
-		ParallelHMMStateState state = (ParallelHMMStateState)
-		    token.getSentenceHMMState();
-		modelName = state.getModelName();
-		*/
-		modelName = token.getModelName();
+                modelName = token.getModelName();
 	    }
 	}
 	return modelName;
-    }
-
-
-    /**
-     * Performs post-recognition cleanup. 
-     */
-    public void stop() {
     }
 }
