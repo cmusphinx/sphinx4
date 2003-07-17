cc3b3472f5ce4f17761d44aa44e777dfbc5cc4fb
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-07-17 20:30:19 +0000

    Added getter methods to the FeatureStream. Also added a factory method to FeatureStream, since there should only be one instance of a FeatureStream of a particular type.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@1722 94700074-3cef-4d97-a70e-9c8c206c02f5

--- edu/cmu/sphinx/research/parallel/FeatureStream.java
+++ edu/cmu/sphinx/research/parallel/FeatureStream.java
@@ -15,35 +15,103 @@ package edu.cmu.sphinx.research.parallel;
 
 import edu.cmu.sphinx.decoder.search.ActiveList;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * Implements a FeatureStream used in parallel decoding.
+ * Each kind of FeatureStream is a singleton (i.e., there is only 
+ * one instance).
  */
 public class FeatureStream {
+
+    private static final Map streams = new HashMap();
 
     /**
      * Name of the FeatureStream.
      */
-    String name;
+    private final String name;
 
     /**
      * Eta value assigned to the FeatureStream.
      */
-    float eta;
+    private float eta;
 
     /**
      * Token ActiveList for this FeatureStream.
      */
-    ActiveList activeList;
+    private ActiveList activeList;
+
+    private FeatureStream(String name) {
+        this.name = name;
+    }
 
     /**
-     * Constructs a FeatureStream with the given name, eta value,
-     * and ActiveList.
+     * Returns a FeatureStream with the given name.
      *
      * @param name the name of the FeatureStream
-     * @param eta the eta value of the FeatureStream
      */
-    FeatureStream(String name, float eta) {
-        this.name = name;
+    public static FeatureStream getFeatureStream(String name) {
+        FeatureStream stream = (FeatureStream)streams.get(name);
+        if (stream == null) {
+            stream = new FeatureStream(name);
+            streams.put(name, stream);
+        }
+        return stream;
+    }
+
+    /**
+     * Returns an iterator of all FeatureStreams
+     *
+     * @return an iterator
+     */
+    public static Iterator iterator() {
+        return streams.values().iterator();
+    }
+
+    /**
+     * Returns the name of this FeatureStream.
+     *
+     * @return the name of this FeatureStream
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the eta value of this FeatureStream.
+     *
+     * @return the eta value of this FeatureStream
+     */
+    public float getEta() {
+        return eta;
+    }
+
+    /**
+     * Returns the active list
+     *
+     * @return the active list
+     */
+    public ActiveList getActiveList() {
+        return activeList;
+    }
+
+    /**
+     * Sets the eta value of this FeatureStream
+     *
+     * @param eta the eta value
+     */
+    public void setEta(float eta) {
         this.eta = eta;
+    }
+
+    /**
+     * Sets the active list
+     *
+     * @param activeList the active list
+     */
+    public void setActiveList(ActiveList list) {
+        this.activeList = list;
     }
 }        
