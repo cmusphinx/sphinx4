651ce9c6e13a04e3f6653b6581f635c80c655855
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-09-24 17:35:25 +0000

    WordSequence contains an array of Word objects instead of wordIDs. The hashCode and equals method have been modified accordingly.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@1918 94700074-3cef-4d97-a70e-9c8c206c02f5

--- edu/cmu/sphinx/knowledge/language/WordSequence.java
+++ edu/cmu/sphinx/knowledge/language/WordSequence.java
@@ -11,6 +11,9 @@
  */
 
 package edu.cmu.sphinx.knowledge.language;
+
+import edu.cmu.sphinx.knowledge.dictionary.Word;
+
 import java.util.List;
 
 
@@ -22,7 +25,7 @@ import java.util.List;
  */
 public class WordSequence {
 
-    private int[] wordIDs;
+    private Word[] words;
     private transient int hashCode = -1;
     public final static WordSequence EMPTY = new WordSequence();
 
@@ -39,18 +42,18 @@ public class WordSequence {
      * @param size the maxium depth of the word history
      */
     private WordSequence(int size) {
-        wordIDs = new int[size];
+        words = new Word[size];
     }
 
     /**
      * Constructs a word sequence with the given word IDs
      *
-     * @param wordIDs the word IDs of the word sequence
+     * @param words the word IDs of the word sequence
      */
-    private WordSequence(int[] wordIDs) {
-        this.wordIDs = new int[wordIDs.length];
-        for (int i = 0; i < wordIDs.length; i++) {
-            this.wordIDs[i] = wordIDs[i];
+    private WordSequence(Word[] words) {
+        this.words = new Word[words.length];
+        for (int i = 0; i < words.length; i++) {
+            this.words[i] = words[i];
         }
         check();
     }
@@ -60,8 +63,8 @@ public class WordSequence {
      *
      * @return a WordSequence with the given word IDs
      */
-    public static WordSequence getWordSequence(int[] wordIDs) {
-        return (new WordSequence(wordIDs));
+    public static WordSequence getWordSequence(Word[] words) {
+        return (new WordSequence(words));
     }
 
     /**
@@ -72,17 +75,16 @@ public class WordSequence {
     public static WordSequence getWordSequence(List list) {
         WordSequence sequence = new WordSequence(list.size());
         for (int i = 0; i < list.size(); i++) {
-            sequence.wordIDs[i] = ((Integer)list.get(i)).intValue();
+            sequence.words[i] = (Word)list.get(i);
         }
         sequence.check();
         return sequence;
     }
 
     private void check() {
-        for (int i = 0; i < wordIDs.length; i++) {
-            if (wordIDs[i] == 0) {
-                throw new Error
-                    ("Word IDs should not be 0. Assign another value.");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i] == null) {
+                throw new Error("WordSequence should not have null Words.");
             }
         }
     }
@@ -94,7 +96,7 @@ public class WordSequence {
      * @param word the word to add to the sequence
      * @param maxSize the maximum size of the generated sequence
      */
-    public WordSequence addWord(int wordID, int maxSize) {
+    public WordSequence addWord(Word word, int maxSize) {
         if (maxSize <= 0) {
             return EMPTY;
         }
@@ -102,10 +104,10 @@ public class WordSequence {
         WordSequence next = new WordSequence(nextSize);
         int nextIndex = nextSize - 1;
         int thisIndex = size() - 1;
-        next.wordIDs[nextIndex--] = wordID;
+        next.words[nextIndex--] = word;
 
         while (nextIndex >= 0 && thisIndex >= 0) {
-            next.wordIDs[nextIndex--] = this.wordIDs[thisIndex--];
+            next.words[nextIndex--] = this.words[thisIndex--];
         }
         next.check();
 
@@ -122,9 +124,9 @@ public class WordSequence {
         WordSequence next = EMPTY;
 
         if (size() >= 1) {
-            next = new WordSequence(wordIDs.length -1);
-            for (int i = 0; i < next.wordIDs.length; i++) {
-                next.wordIDs[i] = this.wordIDs[i];
+            next = new WordSequence(words.length -1);
+            for (int i = 0; i < next.words.length; i++) {
+                next.words[i] = this.words[i];
             }
         }
 	return next;
@@ -140,9 +142,9 @@ public class WordSequence {
         WordSequence next = EMPTY;
 
         if (size() >= 1) {
-            next = new WordSequence(wordIDs.length -1);
-            for (int i = 0; i < next.wordIDs.length; i++) {
-                next.wordIDs[i] = this.wordIDs[i + 1];
+            next = new WordSequence(words.length -1);
+            for (int i = 0; i < next.words.length; i++) {
+                next.words[i] = this.words[i + 1];
             }
         }
 	return next;
@@ -165,11 +167,11 @@ public class WordSequence {
                 maxSize = size();
             }
             WordSequence next = new WordSequence(maxSize);
-            int thisIndex = wordIDs.length - 1;
-            int nextIndex = next.wordIDs.length - 1;
+            int thisIndex = words.length - 1;
+            int nextIndex = next.words.length - 1;
 
             for (int i = 0; i < maxSize; i++) {
-                next.wordIDs[nextIndex--] = this.wordIDs[thisIndex--];
+                next.words[nextIndex--] = this.words[thisIndex--];
             }
             return next;
         }
@@ -182,11 +184,11 @@ public class WordSequence {
      *
      * @return the nth word in this sequence
      */
-    public int getWordID(int n) {
-        if (n >= wordIDs.length) {
+    public Word getWord(int n) {
+        if (n >= words.length) {
             throw new ArrayIndexOutOfBoundsException(n);
         }
-        return wordIDs[n];
+        return words[n];
     }
 
     /**
@@ -195,7 +197,7 @@ public class WordSequence {
      * @return the number of words
      */
     public int size() {
-        return wordIDs.length;
+        return words.length;
     }
 
     /**
@@ -206,9 +208,9 @@ public class WordSequence {
      */
     public String toString() {
 	StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < wordIDs.length; i++) {
+        for (int i = 0; i < words.length; i++) {
             sb.append("[");
-            sb.append(wordIDs[i]);
+            sb.append(words[i].toString());
             sb.append("]");
 	}
 	return sb.toString();
@@ -222,11 +224,11 @@ public class WordSequence {
      */
     public String toText() {
 	StringBuffer sb = new StringBuffer(20);
-	for (int i = 0; i < wordIDs.length; i++) {
+	for (int i = 0; i < words.length; i++) {
 	    if (i != 0) {
 		sb.append(" ");
 	    }
-	    sb.append(wordIDs[i]);
+	    sb.append(words[i].toString());
 	}
 	return sb.toString();
     }
@@ -239,8 +241,8 @@ public class WordSequence {
     public int hashCode() {
 	if (hashCode == -1) {
 	    int code = 123;
-	    for (int i = 0; i < wordIDs.length; i++) {
-                code += wordIDs[i] * (i + 1);
+	    for (int i = 0; i < words.length; i++) {
+                code += words[i].hashCode() * (i + 1);
 	    }
 	    hashCode = code;
 	}
@@ -260,9 +262,9 @@ public class WordSequence {
             return true;
         } else if (o instanceof WordSequence) {
 	    WordSequence other = (WordSequence) o;
-            if (this.wordIDs.length == other.wordIDs.length) {
-                for (int i = 0; i < this.wordIDs.length; i++) {
-                    if (this.wordIDs[i] != other.wordIDs[i]) {
+            if (words.length == other.words.length) {
+                for (int i = 0; i < words.length; i++) {
+                    if (!words[i].equals(other.words[i])) {
                         return false;
                     }
                 }
