b4117c1154c502a394edbaf352d542cd43a12bf7
lamere <lamere@94700074-3cef-4d97-a70e-9c8c206c02f5>
2003-10-15 09:31:22 +0000

    Added missing javadocs
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@2069 94700074-3cef-4d97-a70e-9c8c206c02f5

--- edu/cmu/sphinx/decoder/linguist/clextree/HMMTree.java
+++ edu/cmu/sphinx/decoder/linguist/clextree/HMMTree.java
@@ -35,8 +35,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 
-
-
 /**
  * Represents the vocabulary as a lex tree with nodes in the tree
  * representing either words (WordNode) or units (HMMNode). HMMNodes
@@ -78,6 +76,9 @@ class HMMTree {
     }
 
 
+    /**
+     * Compiles the vocabulary into an HMM Tree
+     */
     private void compile() {
         collectEntryAndExitUnits();
         entryPointTable = new EntryPointTable(entryPoints);
@@ -94,11 +95,7 @@ class HMMTree {
     /**
      * Dumps the tree
      *
-     * @param level the level of the dump
-     * @param node the root of the tree to dump
      */
-
-
     void dumpTree() {
         System.out.println("Dumping Tree ...");
         Map dupNode = new HashMap();
@@ -106,7 +103,14 @@ class HMMTree {
         System.out.println("... done Dumping Tree");
     }
 
-    void dumpTree(int level, Node node, Map dupNode) {
+    /**
+     * Dumps the tree
+     *
+     * @param level the level of the dump
+     * @param node the root of the tree to dump
+     * @param map map of visited nodes
+     */
+    private void dumpTree(int level, Node node, Map dupNode) {
         if (dupNode.get(node) == null) {
             dupNode.put(node, node);
             System.out.println(Utilities.pad(level * 1) + node);
@@ -118,6 +122,9 @@ class HMMTree {
         }
     }
 
+    /**
+     * Collects all of the entry and exit points for the vocabulary.
+     */
     private void collectEntryAndExitUnits() {
         Collection words = lm.getVocabulary();
         for (Iterator i = words.iterator(); i.hasNext(); ) {
@@ -145,12 +152,9 @@ class HMMTree {
         }
     }
 
-    private void buildEntryPointTable() {
-        // create the entry point table and add an entry for each
-        // entry point.
-
-    }
-
+    /**
+     * Connects all of the words to the beginning of the lex tree
+     */
     private void connectWords() {
 	for (Iterator i = wordSet.iterator(); i.hasNext(); ) {
 	    WordNode wordNode = (WordNode) i.next();
@@ -161,16 +165,21 @@ class HMMTree {
 	}
     }
 
+    /**
+     * Connects a single word to the beginning of the lex tree
+     *
+     * @param word the word to connect.
+     */
+    private void connectWordToStart(WordNode word) {
     // we have a word represented by the given tail node. This word 
     // needs to connect up with all of the possible entry points, 
     // while respecting the left and base context.
 
     // NOTE: there is likely some large opportunity for sharing here
     // that will greatly reduce memory requirements. Coming soon
-    private void connectWordToStart(WordNode tail) {
 	int count = 0;
-	Unit lc = tail.getLastUnit();
-	for (Iterator i = tail.getRC().iterator(); i.hasNext(); ) {
+	Unit lc = word.getLastUnit();
+	for (Iterator i = word.getRC().iterator(); i.hasNext(); ) {
 	    Unit nextBase = (Unit) i.next();
 	    EntryPoint ep = entryPointTable.getEntryPoint(nextBase);
 	    Node entryPointNode = ep.getEntryPointsFromLeftContext(lc);
@@ -178,25 +187,30 @@ class HMMTree {
 	    for (Iterator j = entryPointNode.getSuccessors().iterator(); 
 		    j.hasNext(); ) {
 		HMMNode epNode = (HMMNode) j.next();
-		tail.addSuccessor(epNode);
+		word.addSuccessor(epNode);
 		count++;
 	    }
 	}
 	if (false) {
 	    System.out.println("    Added " + count + " tails to " 
-		+ tail.getPronunciation());
+		+ word.getPronunciation());
 	}
     }
 
 
-
+    /**
+     * Called after the lex tree is built. Frees all temporary
+     * structures. After this is called, no more words can be added to
+     * the lex tree.
+     */
     private void freeze() {
+        // TODO: write me
+        // This is an opportunity for space optimizing 
     }
 
     /**
      * Adds the given collection of words to the lex tree
      *
-     * @param words the collection of words
      */
     private void addWords() {
         Collection words = lm.getVocabulary();
@@ -222,7 +236,7 @@ class HMMTree {
     /**
      * Adds a single word to the lex tree
      *
-     * @param spelling the word to add
+     * @param word the word to add
      */
     private void addWord(Word word) {
         Pronunciation[] pronunciations = word.getPronunciations();
@@ -276,6 +290,19 @@ class HMMTree {
 
 
 
+    /**
+     * Retrieves an HMM for a unit in context. If there is no direct
+     * match, the nearest match will be used.  Note that we are
+     * currently only dealing with, at most, single unit left 
+     * and right contexts.
+     *
+     * @param base the base CI unit
+     * @param lc the left context
+     * @param rc the right context
+     * @param pos the position of the base unit within the word
+     *
+     * @return the HMM. (This should never return null)
+     */
     private HMM getHMM(Unit base, Unit lc, Unit rc, HMMPosition pos) {
         int id = hmmPool.buildID(hmmPool.getID(base), hmmPool.getID(lc), 
                                  hmmPool.getID(rc));
@@ -294,7 +321,6 @@ class HMMTree {
     }
 
 
-
     /**
      * Returns the initial node for this lex tree
      *
@@ -306,19 +332,45 @@ class HMMTree {
         return initialNode;
     }
 
+    /**
+     * The EntryPoint table is used to manage the set of entry points
+     * into the lex tree. 
+     */
     class EntryPointTable {
 	private Map entryPoints;
-	EntryPointTable(Collection eps) {
+
+
+        /**
+         * Create the entry point table give the set of all possible
+         * entry point units
+         *
+         * @param entryPointCollection the set of possible entry
+         * points
+         */
+	EntryPointTable(Collection entryPointCollection) {
 	    entryPoints = new HashMap();
-	    for (Iterator i = eps.iterator(); i.hasNext(); ) {
+	    for (Iterator i = entryPointCollection.iterator(); i.hasNext(); ) {
 		Unit unit = (Unit) i.next();
 		entryPoints.put(unit, new EntryPoint(unit));
 	    }
 	}
+
+        /**
+         * Given a CI unit, return the EntryPoint object that manages
+         * the entry point for the unit
+         *
+         * @param baseUnit the unit of interest (A ci unit)
+         *
+         * @return the object that manages the entry point for the
+         * unit
+         */
 	EntryPoint getEntryPoint(Unit baseUnit) {
 	    return (EntryPoint) entryPoints.get(baseUnit);
 	}
 
+        /**
+         * Creates the entry point maps for all entry points. 
+         */
 	void createEntryPointMaps() {
 	    for (Iterator i = entryPoints.values().iterator(); i.hasNext(); ) {
 		EntryPoint ep = (EntryPoint) i.next();
@@ -326,15 +378,10 @@ class HMMTree {
 	    }
 	}
 
-	List collectWordNodes() {
-	    List list = new ArrayList();
-	    for (Iterator i = entryPoints.values().iterator(); i.hasNext(); ) {
-		EntryPoint ep = (EntryPoint) i.next();
-		ep.collectWordNodes(list);
-	    }
-	    return list;
-	}
 
+        /**
+         * Dumps the entry point table
+         */
 	void dump() {
 	    for (Iterator i = entryPoints.values().iterator(); i.hasNext(); ) {
 		EntryPoint ep = (EntryPoint) i.next();
@@ -344,6 +391,9 @@ class HMMTree {
     }
 
 
+    /**
+     * Manages a single entry point.
+     */
     class EntryPoint {
 	Unit baseUnit;
 	Node baseNode;      // second units and beyond start here
@@ -352,6 +402,11 @@ class HMMTree {
 	int nodeCount = 0;
 	Set rcSet;
 
+        /**
+         * Creates an entry point for the given usnit
+         *
+         * @param baseUnit the EntryPoint is created for this unit
+         */
 	EntryPoint(Unit baseUnit) {
 	    this.baseUnit = baseUnit;
 	    this.baseNode = new Node();
@@ -359,20 +414,45 @@ class HMMTree {
 	    this.singleUnitWords = new ArrayList();
 	}
 
+        /**
+         * Given a left context get a node that represents a single
+         * set of entry points into this unit
+         *
+         * @param leftContext the left context of interest
+         *
+         * @return the node representing the entry point
+         */
 	Node getEntryPointsFromLeftContext(Unit leftContext) {
 	    return (Node) unitToEntryPointMap.get(leftContext);
 	}
 
+        /**
+         * Gets the base node for this entry point
+         *
+         * @return the base node
+         */
 	Node getNode() {
 	    return baseNode;
 	}
 
 
+        /**
+         * Adds a one-unit word to this entry point. Such single unit
+         * words need to be dealt with specially.
+         *
+         * @param p the pronunciation of the single unit word
+         */
 	void addSingleUnitWord(Pronunciation p) {
 	    singleUnitWords.add(p);
 	}
 
-	Collection getRC() {
+        /**
+         * Gets the set of possible right contexts that we can
+         * transition to from this entry point
+         *
+         * @return the set of possible transition points.
+         */
+	private Collection getRC() {
 	    if (rcSet == null) {
 		rcSet = new HashSet();
 	    }
@@ -385,6 +465,14 @@ class HMMTree {
 	    return  rcSet;
 	}
 
+        /**
+         * Creates the entry point map for this entry point.  The
+         * entry point map is represented by the unitToEntryPointMap.
+         * It contains a node for each possible left context. The node
+         * successors point to the following hmm nodes (usually
+         * associated with the next units that can follow from this
+         * entry point.
+         */
 	void createEntryPointMap() {
 	    for (Iterator i = exitPoints.iterator(); i.hasNext(); ) {
 		Unit lc = (Unit) i.next();
@@ -402,6 +490,17 @@ class HMMTree {
 	}
 
 
+        /**
+         * Connects the single unit words associated with this entry
+         * point.   The singleUnitWords list contains all single unit
+         * pronunciations that have as their sole unit, the unit
+         * associated with this entry point. Entry points for these
+         * words are added to the epNode for all possible left (exit)
+         * and right (entry) contexts.
+         *
+         * @param lc the left context
+         * @param epNode the entry point node
+         */
 	private void connectSingleUnitWords(Unit lc, Node epNode) {
 	    if (singleUnitWords.size() > 0) {
 		for (Iterator i = entryPoints.iterator(); i.hasNext(); ) {
@@ -429,6 +528,14 @@ class HMMTree {
 	    }
 	}
 
+        /**
+         * Connect the entry points that match the given rc to the
+         * given epNode
+         *
+         * @param epNode add matching successors here
+         * @param rc the next unit
+         *
+         */ 
 	private void connectEntryPointNode(Node epNode, Unit rc) {
 	    for (Iterator i = baseNode.getSuccessors().iterator(); 
 					    i.hasNext(); ) {
@@ -439,23 +546,11 @@ class HMMTree {
 	    }
 	}
 
-	void collectWordNodes(List list ) {
-	    collectWordNodes(list, baseNode);
-	}
-
-	private void collectWordNodes(List list, Node node) {
-	    if (node instanceof WordNode) {
-		list.add(node);
-	    } else {
-		for (Iterator i = node.getSuccessors().iterator();
-			i.hasNext(); ) {
-		    Node nextNode = (Node) i.next();
-		    collectWordNodes(list, nextNode);
-		}
-	    }
-	}
 
 
+        /**
+         * Dumps the entry point
+         */
 	void dump() {
 	    System.out.println("EntryPoint " + baseUnit + " RC Followers: " 
 			    + getRC().size());
@@ -476,20 +571,31 @@ class HMMTree {
     }
 
 }
-class Node {
-    static int nodeCount = 0;
-    Map successors; // the set of successors
 
+/**
+ * Represents a node in the HMM Tree
+ */
+
+class Node {
+    private static int nodeCount = 0;
+    private Map successors; // the set of successors
+
+    /**
+     * Creates a node
+     */
     Node() {
         successors = new HashMap();
     }
 
 
     /**
-     * Adds a child node to the successor.  If a node similar to
+     * Adds a child node holding an hmm to the successor.  If a node similar to
      * the child has already been added, we use the previously
      * added node, otherwise we add this. Also, we record the base
      * unit of the child in the set of right context
+     *
+     * @param hmm the hmm to add
+     * @return the node that holds the hmm (new or old)
      */
     Node addSuccessor(HMM hmm) {
         Node child = null;
@@ -504,10 +610,15 @@ class Node {
     }
 
     /**
-     * Adds a child node to the successor.  If a node similar to
+     * Adds a child node holding a pronunciation to the successor.  
+     * If a node similar to
      * the child has already been added, we use the previously
      * added node, otherwise we add this. Also, we record the base
      * unit of the child in the set of right context
+     *
+     * @param pronunciation the pronunciation to add
+     *
+     * @return the node that holds the pronunciation (new or old)
      */
     WordNode addSuccessor(Pronunciation pronunciation) {
         WordNode child = null;
@@ -527,6 +638,11 @@ class Node {
      * the child has already been added, we use the previously
      * added node, otherwise we add this. Also, we record the base
      * unit of the child in the set of right context
+     *
+     * @param child the child to add
+     * @return the node (may be different than child if there was
+     * already a node attached holding the hmm held by child)
+     *
      */
     HMMNode addSuccessor(HMMNode child) {
         HMMNode matchingChild = (HMMNode) successors.get(child.getHMM());
@@ -539,13 +655,26 @@ class Node {
     }
 
 
+    /**
+     * Freezes this node (not implemented yet).
+     */
     void freeze() {
     }
 
+    /**
+     * Returns the successors for this node
+     *
+     * @return the set of successor nodes
+     */
     Collection getSuccessors() {
         return successors.values();
     }
 
+    /**
+     * Returns the string representation fo this object
+     *
+     * @return the string representation of the object
+     */
     public String toString() {
         return "Node ";
     }
@@ -553,59 +682,124 @@ class Node {
 }
 
 
+/**
+ * A node representing a word in the hmm tree
+ *
+ */
 class WordNode extends Node {
-    Pronunciation pronunciation;
-    HMMNode parent;
+    private Pronunciation pronunciation;
+    private HMMNode parent;
 
+    /**
+     * Creates a word node
+     *
+     * @param pronunciation the pronunciation to wrap in this node
+     * @param parent the parent node
+     */
     WordNode(Pronunciation pronunciation, HMMNode parent) {
         this.pronunciation = pronunciation;
         this.parent = parent;
     }
 
 
+    /**
+     * Gets the word associated with this node
+     *
+     * @return the word
+     */
     Word getWord() {
         return pronunciation.getWord();
     }
 
+    /**
+     * Gets the pronunciation associated with this node
+     *
+     * @return the pronunciation
+     */
     Pronunciation getPronunciation() {
         return pronunciation;
     }
 
+    /**
+     * Gets the set of possible next units for this node
+     *
+     * @return the set of next units
+     */
     Collection getRC() {
         return parent.getRC();
     }
 
+    /**
+     * Gets the last unit for this word
+     *
+     * @return the last unit
+     */
     Unit getLastUnit() {
 	Unit[] units = pronunciation.getUnits();
 	return units[units.length - 1];
     }
 
 
+    /**
+     * Returns a string representation for this object
+     *
+     * @return a string representation
+     */
     public String toString() {
         return "WordNode " + pronunciation;
     }
 }
 
+/**
+ * A node that represents an HMM in the hmm tree
+ */
 class HMMNode extends Node {
     private HMM hmm;
     private Set rcSet;
 
+    /**
+     * Creates the node, wrapping the given hmm
+     *
+     * @param hmm the hmm to hold
+     */
     HMMNode(HMM hmm) {
         this.hmm = hmm;
     }
 
+    /**
+     * Returns the base unit for this hmm node
+     *
+     * @return the base unit
+     */
     Unit getBaseUnit() {
         return hmm.getBaseUnit();
     }
 
+    /**
+     * Returns the hmm for this node
+     *
+     * @return the hmm
+     */
     HMM getHMM() {
         return hmm;
     }
 
+    /**
+     * Returns a string representation for this object
+     *
+     * @return a string representation
+     */
     public String toString() {
         return "HMMNode " + hmm;
     }
 
+    /**
+     * Adds a right context to the set of possible right contexts for
+     * this node. This is typically only needed for hmms at the ends
+     * of words.
+     *
+     * @param rc the right context.
+     */
     void addRC(Unit rc) {
 	if (rcSet == null) {
 	    rcSet = new HashSet();
@@ -613,6 +807,11 @@ class HMMNode extends Node {
 	rcSet.add(rc);
     }
 
+    /**
+     * returns the set of right contexts for this node
+     *
+     * @return the set of right contexts
+     */
     Collection getRC() {
 	return rcSet;
     }
