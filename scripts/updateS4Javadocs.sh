d255cb0a27c4b0247db972331079f5736182e412
egouvea <egouvea@94700074-3cef-4d97-a70e-9c8c206c02f5>
2004-02-04 23:01:33 +0000

    Added script to push javadoc into sf.net and report failures
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@2505 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ scripts/updateS4Javadocs.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+. ${HOME}/.profile
+
+JAVA_HOME=/lab/speech/java/j2sdk1.4.2
+export JAVA_HOME
+
+PUSH_DEST=/home/groups/c/cm/cmusphinx/htdocs/sphinx4
+export PUSH_DEST
+
+DOC_DEST=bld/javadoc
+export DOC_DEST
+
+JAVADOC_OUT=javadoc.out
+
+LIST=cmusphinx-commits@lists.sourceforge.net
+
+cd /tmp
+cvs -d:ext:cvs.sourceforge.net:/cvsroot/cmusphinx co sphinx4
+cd sphinx4
+ant javadoc
+gtar cf /tmp/sphinx4docs.tar -C ${DOC_DEST} .
+${HOME}/bin/sscp /tmp/sphinx4docs.tar
+${HOME}/bin/sshh tar xf sphinx4docs.tar -C ${PUSH_DEST}
+rm /tmp/sphinx4docs.tar
+ant -emacs -Daccess=private clean javadoc > ${JAVADOC_OUT}
+if grep -w -i warning ${JAVADOC_OUT}; then mailx -s "Javadoc result" ${LIST} < ${JAVADOC_OUT};
+elif grep -i error ${JAVADOC_OUT}; then mailx -s "Javadoc result" ${LIST} < ${JAVADOC_OUT};
+fi
+
+cd /tmp
+rm -rf sphinx4
+
