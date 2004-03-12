9f8defc0af2140ecb041b925ce22ca71a18af173
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2004-03-12 17:06:39 +0000

    Added the method getDuration().
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@2745 94700074-3cef-4d97-a70e-9c8c206c02f5

--- edu/cmu/sphinx/frontend/DataEndSignal.java
+++ edu/cmu/sphinx/frontend/DataEndSignal.java
@@ -22,20 +22,35 @@ package edu.cmu.sphinx.frontend;
  */
 public class DataEndSignal extends Signal {
 
+    private long duration;
+
     /**
      * Constructs a DataEndSignal.
+     *
+     * @param duration the duration of the entire data stream in milliseconds
      */
-    public DataEndSignal() {
-        this(System.currentTimeMillis());
+    public DataEndSignal(long duration) {
+        this(duration, System.currentTimeMillis());
     }
 
     /**
      * Constructs a DataEndSignal with the given creation time.
      *
+     * @param duration the duration of the entire data stream in milliseconds 
      * @param time the creation time of the DataEndSignal
      */
-    public DataEndSignal(long time) {
+    public DataEndSignal(long duration, long time) {
         super(time);
+        this.duration = duration;
+    }
+
+    /**
+     * Returns the duration of the entire data stream in milliseconds
+     *
+     * @return the duration of the entire data stream in milliseconds
+     */
+    public long getDuration() {
+        return duration;
     }
 
     /**
@@ -44,6 +59,7 @@ public class DataEndSignal extends Signal {
      * @return the string "DataEndSignal"
      */
     public String toString() {
-        return "DataEndSignal";
+        return ("DataEndSignal: creation time: " + getTime() + ", duration: " +
+                getDuration() + "ms");
     }
 }
