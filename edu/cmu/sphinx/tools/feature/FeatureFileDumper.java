eddea62eeb17e97c22f1aac5f3786d99102065c5
lamere <lamere@94700074-3cef-4d97-a70e-9c8c206c02f5>
2004-03-23 12:11:04 +0000

    Converted the feature file dumper to use the new configuration manager
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@2865 94700074-3cef-4d97-a70e-9c8c206c02f5

--- edu/cmu/sphinx/tools/feature/FeatureFileDumper.java
+++ edu/cmu/sphinx/tools/feature/FeatureFileDumper.java
@@ -10,7 +10,6 @@
  *
  */
 
-
 package edu.cmu.sphinx.tools.feature;
 
 import java.io.DataOutputStream;
@@ -21,7 +20,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.URL;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -32,19 +30,17 @@ import edu.cmu.sphinx.frontend.DataEndSignal;
 import edu.cmu.sphinx.frontend.DoubleData;
 import edu.cmu.sphinx.frontend.FloatData;
 import edu.cmu.sphinx.frontend.FrontEnd;
-import edu.cmu.sphinx.frontend.FrontEndFactory;
 import edu.cmu.sphinx.frontend.util.StreamDataSource;
-import edu.cmu.sphinx.util.SphinxProperties;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
+import edu.cmu.sphinx.util.props.PropertyException;
 
 /**
  * This program takes in an audio file, does frontend signal processing to it,
  * and then dumps the resulting Feature into a separate file.
- *
- * This program takes three arguments:
- *
- * 1. propsFile - the Sphinx properties file
- * 2. audioFile - the name of the audio file
- * 3. outputFile - the name of the output file
+ * 
+ * This program takes three arguments: 1. propsFile - the Sphinx properties
+ * file 2. audioFile - the name of the audio file 3. outputFile - the name of
+ * the output file
  */
 public class FeatureFileDumper {
 
@@ -56,108 +52,96 @@ public class FeatureFileDumper {
     /**
      * The logger for this class
      */
-    private static Logger logger =
-            Logger.getLogger("edu.cmu.sphinx.tools.feature.FeatureFileDumper");
+    private static Logger logger = Logger
+            .getLogger("edu.cmu.sphinx.tools.feature.FeatureFileDumper");
 
     /**
      * Constructs a FeatureFileDumper.
-     *
-     * @param propsFile the Sphinx properties file
-     * @param inputAudioFile the input audio file
+     * 
+     * @param propsFile
+     *                the Sphinx properties file
+     * @param inputAudioFile
+     *                the input audio file
      */
-    public FeatureFileDumper(String propsFile, String inputAudioFile)
-	throws FileNotFoundException, IOException {
-	try {
-            /*
-	     * Create a new front end and get the feature (cepstra, plp, etc).
-             */
-	    String context = "featureDumping";
-            URL url = new File(propsFile).toURI().toURL();
-            SphinxProperties.initContext (context, url);
-	    SphinxProperties props = SphinxProperties.getSphinxProperties
-		(context);
+    public FeatureFileDumper(ConfigurationManager cm, String frontEndName,
+            String inputAudioFile)
+            throws FileNotFoundException, IOException {
+        try {
 
-	    StreamDataSource audioSource = new StreamDataSource();
-	    audioSource.initialize("BatchFileAudioSource", context,
-				   props, null);
-	    audioSource.setInputStream(new FileInputStream(inputAudioFile), 
-		"audio");
-
-	    Collection names = FrontEndFactory.getNames(props);
-	    assert (names.size() == 1);
-	    String feName = (String) names.iterator().next();
-
-            frontEnd = FrontEndFactory.getFrontEnd(props, feName);
-            frontEnd.setDataSource(audioSource);
-
-
-	    allFeatures = new LinkedList();
-	    getAllFeatures();
-	    logger.info("Frames: " + allFeatures.size());
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+            frontEnd = (FrontEnd) cm.lookup(frontEndName);
+            StreamDataSource audioSource = (StreamDataSource) cm
+                    .lookup("streamDataSource");
+            audioSource.setInputStream(new FileInputStream(inputAudioFile),
+                    "audio");
+            allFeatures = new LinkedList();
+            getAllFeatures();
+            logger.info("Frames: " + allFeatures.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
-     * Retrieve all Features from the frontend, and cache all those
-     * with actual feature data.
+     * Retrieve all Features from the frontend, and cache all those with actual
+     * feature data.
      */
     private void getAllFeatures() {
-	/* Run through all the data and produce feature.
-	 */
-	try {
-	    assert(allFeatures != null);
-	    Data feature = frontEnd.getData();
-	    while (!(feature instanceof DataEndSignal)) {
-		if (feature instanceof DoubleData) {
-		    double[] featureData = ((DoubleData)feature).getValues();
-		    if (featureLength < 0) {
-			featureLength = featureData.length;
+        /*
+         * Run through all the data and produce feature.
+         */
+        try {
+            assert(allFeatures != null);
+            Data feature = frontEnd.getData();
+            while (!(feature instanceof DataEndSignal)) {
+                if (feature instanceof DoubleData) {
+                    double[] featureData = ((DoubleData) feature).getValues();
+                    if (featureLength < 0) {
+                        featureLength = featureData.length;
                         logger.info("Feature length: " + featureLength);
-		    }
-		    allFeatures.add(featureData);
-		} else if (feature instanceof FloatData) {
-                    float[] featureData = ((FloatData)feature).getValues();
+                    }
+                    allFeatures.add(featureData);
+                } else if (feature instanceof FloatData) {
+                    float[] featureData = ((FloatData) feature).getValues();
                     if (featureLength < 0) {
                         featureLength = featureData.length;
                         logger.info("Feature length: " + featureLength);
                     }
                     allFeatures.add(featureData);
                 }
-		feature = frontEnd.getData();
-	    }
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+                feature = frontEnd.getData();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
-     * Returns the total number of data points that should be written
-     * to the output file.
-     *
+     * Returns the total number of data points that should be written to the
+     * output file.
+     * 
      * @return the total number of data points that should be written
      */
     private int getNumberDataPoints() {
-	return (allFeatures.size() * featureLength);
+        return (allFeatures.size() * featureLength);
     }
 
     /**
      * Dumps the feature to the given binary output.
-     *
-     * @param outputFile the binary output file
+     * 
+     * @param outputFile
+     *                the binary output file
      */
     public void dumpBinary(String outputFile) throws IOException {
-	DataOutputStream outStream = new DataOutputStream
-	    (new FileOutputStream(outputFile));
-	outStream.writeInt(getNumberDataPoints());
+        DataOutputStream outStream = new DataOutputStream(new FileOutputStream(
+                outputFile));
+        outStream.writeInt(getNumberDataPoints());
 
-	for (Iterator i = allFeatures.iterator(); i.hasNext();) {
+        for (Iterator i = allFeatures.iterator(); i.hasNext();) {
             Object data = i.next();
             if (data instanceof double[]) {
-                double[] feature = (double []) data;
+                double[] feature = (double[]) data;
                 for (int d = 0; d < feature.length; d++) {
-                    outStream.writeFloat((float)feature[d]);
+                    outStream.writeFloat((float) feature[d]);
                 }
             } else if (data instanceof float[]) {
                 float[] feature = (float[]) data;
@@ -165,72 +149,76 @@ public class FeatureFileDumper {
                     outStream.writeFloat(feature[d]);
                 }
             }
-	}
+        }
 
-	outStream.close();
+        outStream.close();
     }
 
     /**
      * Dumps the feature to the given ascii output file.
-     *
-     * @param outputFile the ascii output file
+     * 
+     * @param outputFile
+     *                the ascii output file
      */
     public void dumpAscii(String outputFile) throws IOException {
-	PrintStream ps = new PrintStream(new FileOutputStream(outputFile),
-					 true);
-	ps.print(getNumberDataPoints());
-	ps.print(' ');
+        PrintStream ps = new PrintStream(new FileOutputStream(outputFile), true);
+        ps.print(getNumberDataPoints());
+        ps.print(' ');
 
         for (Iterator i = allFeatures.iterator(); i.hasNext();) {
-	    double[] feature = (double []) i.next();
+            double[] feature = (double[]) i.next();
             for (int d = 0; d < feature.length; d++) {
-		ps.print(feature[d]);
-		ps.print(' ');
-		}
+                ps.print(feature[d]);
+                ps.print(' ');
+            }
         }
 
-	ps.close();
+        ps.close();
     }
 
     /**
      * Main program for this dumper.
-     *
-     * This program takes three arguments:
-     *
-     * 1. propsFile - the Sphinx properties file
-     * 2. audioFile - the name of the audio file
-     * 3. outputFile - the name of the binary output file
+     * 
+     * This program takes three arguments: 1. propsFile - the Sphinx properties
+     * file 2. audioFile - the name of the audio file 3. outputFile - the name
+     * of the binary output file
      */
     public static void main(String[] argv) {
 
-	if (argv.length < 3) {
-	    System.out.println("Usage: FeatureFileDumper " + 
-			       "propsFile inputFile outputFile [(binary|ascii)]");
-	    System.exit(1);
-	}
-	String propsFile = argv[0];
-	String inputFile = argv[1];
-	String outputFile = argv[2];
-	String format = "binary";
-	if (argv.length == 4) {
-	    format = argv[3];
-	}
-	logger.info("Input file: " + inputFile);
-	logger.info("Output file: " + outputFile);
-	logger.info("Format: " + format);
+        if (argv.length < 3) {
+            System.out
+                    .println("Usage: FeatureFileDumper "
+                            + "configFile frontendName inputFile outputFile [(binary|ascii)]");
+            System.exit(1);
+        }
+        String configFile = argv[0];
+        String frontEndName = argv[1];
+        String inputFile = argv[2];
+        String outputFile = argv[3];
+        String format = "binary";
+        if (argv.length == 5) {
+            format = argv[4];
+        }
+        logger.info("Input file: " + inputFile);
+        logger.info("Output file: " + outputFile);
+        logger.info("Format: " + format);
 
-	try {
-	    FeatureFileDumper dumper = new FeatureFileDumper
-		(propsFile, inputFile);
-	    if (format.equals("binary")) {
-		dumper.dumpBinary(outputFile);
-	    } else if (format.equals("ascii")) {
-		dumper.dumpAscii(outputFile);
-	    } else {
-		System.out.println("ERROR: unknown output format: " + format);
-	    }
-	} catch (IOException ioe) {
-	    ioe.printStackTrace();
-	}
+        try {
+            URL url = new File(configFile).toURI().toURL();
+            ConfigurationManager cm = new ConfigurationManager(url);
+            FeatureFileDumper dumper = new FeatureFileDumper(cm, frontEndName,
+                    inputFile);
+            if (format.equals("binary")) {
+                dumper.dumpBinary(outputFile);
+            } else if (format.equals("ascii")) {
+                dumper.dumpAscii(outputFile);
+            } else {
+                System.out.println("ERROR: unknown output format: " + format);
+            }
+        } catch (IOException ioe) {
+            System.err.println("I/O Error " + ioe);
+        } catch (PropertyException p) {
+            System.err.println("Bad configuration " + p);
+        }
     }
 }
