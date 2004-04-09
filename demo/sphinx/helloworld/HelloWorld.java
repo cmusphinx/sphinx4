b3397537521c61fee7390b16f9e3ebe980ec57fb
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2004-04-09 17:57:02 +0000

    Made HelloWorld not a Configurable. Moved all the code to a short and simple main() method.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@3013 94700074-3cef-4d97-a70e-9c8c206c02f5

--- demo/sphinx/helloworld/HelloWorld.java
+++ demo/sphinx/helloworld/HelloWorld.java
@@ -13,17 +13,10 @@
 package demo.sphinx.helloworld;
 
 import edu.cmu.sphinx.frontend.util.Microphone;
-
 import edu.cmu.sphinx.recognizer.Recognizer;
-
 import edu.cmu.sphinx.result.Result;
-
-import edu.cmu.sphinx.util.props.Configurable;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 import edu.cmu.sphinx.util.props.PropertyException;
-import edu.cmu.sphinx.util.props.PropertySheet;
-import edu.cmu.sphinx.util.props.PropertyType;
-import edu.cmu.sphinx.util.props.Registry;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,103 +27,7 @@ import java.net.URL;
  * A simple HelloWorld demo showing a simple speech application 
  * built using Sphinx-4.
  */
-public class HelloWorld implements Configurable {
-
-    /**
-     * The sphinx property for the recognizer to use
-     */
-    public static final String PROP_RECOGNIZER = "recognizer";
-
-    /**
-     * The sphinx property for the microphone to use
-     */
-    public static final String PROP_MICROPHONE = "microphone";
-
-    
-    private String name;
-    private Recognizer recognizer;
-    private Microphone microphone;
-
-
-    /**
-     * (non-Javadoc)
-     *
-     * @see edu.cmu.sphinx.util.props.Configurable#register(java.lang.String,
-     *      edu.cmu.sphinx.util.props.Registry)
-     */
-    public void register(String name, Registry registry)
-        throws PropertyException {
-        this.name = name;
-        registry.register(PROP_RECOGNIZER, PropertyType.COMPONENT);
-        registry.register(PROP_MICROPHONE, PropertyType.COMPONENT);
-    }
-
-
-    /**
-     * (non-Javadoc)
-     *
-     * @see edu.cmu.sphinx.util.props.Configurable#newProperties(edu.cmu.sphinx.util.props.PropertySheet)
-     */
-    public void newProperties(PropertySheet ps) throws PropertyException {
-        recognizer = (Recognizer) ps.getComponent(PROP_RECOGNIZER,
-                                                  Recognizer.class);
-        microphone = (Microphone) ps.getComponent(PROP_MICROPHONE,
-                                                  Microphone.class);
-    }
-
-
-    /**
-     * (non-Javadoc)
-     *
-     * @see edu.cmu.sphinx.util.props.Configurable#getName()
-     */
-    public String getName() {
-        return name;
-    }
-
-    
-    /**
-     * Initialize the hello world demo by loading the recognizer.
-     */
-    public void initialize() {
-        try {
-            recognizer.allocate();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
-
-
-    /**
-     * Runs the hello world demo.
-     */
-    public void run() {
-        if (microphone.startRecording()) {
-            System.out.println
-                ("Say any digit(s): e.g. \"two oh oh four\", " +
-                 "\"three six five\".");
-            while (true) {
-                System.out.println
-                    ("Start speaking. " + 
-                     "Press Ctrl-C or say 'good bye' to quit.");
-                Result result = recognizer.recognize();
-                        
-                if (result != null) {
-                    String resultText = result.getBestResultNoFiller();
-                    System.out.println("You said: " + resultText + "\n");
-                    if (resultText.equals("good bye")) {
-                        System.exit(0);
-                    }
-                } else {
-                    System.out.println("I can't hear what you said.\n");
-                }
-            }
-        } else {
-            System.out.println("Cannot start microphone.");
-            System.exit(1);
-        }
-    }
-
+public class HelloWorld {
 
     /**
      * Main method for running the HelloWorld demo.
@@ -143,10 +40,44 @@ public class HelloWorld implements Configurable {
             } else {
                 url = HelloWorld.class.getResource("helloworld.config.xml");
             }
+
             ConfigurationManager cm = new ConfigurationManager(url);
-            HelloWorld helloWorld = (HelloWorld) cm.lookup("helloworld");
-            helloWorld.initialize();
-            helloWorld.run();
+
+	    Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+	    Microphone microphone = (Microphone) cm.lookup("microphone");
+
+            recognizer.allocate();
+
+	    if (microphone.startRecording()) {
+		System.out.println
+		    ("Say any digit(s): e.g. \"two oh oh four\", " +
+		     "\"three six five\".");
+
+		while (true) {
+		    System.out.println
+			("Start speaking. " + 
+			 "Press Ctrl-C or say 'good bye' to quit.");
+
+		    Result result = recognizer.recognize();
+		    
+		    if (result != null) {
+			String resultText = result.getBestResultNoFiller();
+			System.out.println("You said: " + resultText + "\n");
+
+			if (resultText.equals("good bye")) {
+			    microphone.stopRecording();
+			    recognizer.deallocate();
+			    System.exit(0);
+			}
+		    } else {
+			System.out.println("I can't hear what you said.\n");
+		    }
+		}
+	    } else {
+		System.out.println("Cannot start microphone.");
+		recognizer.deallocate();
+		System.exit(1);
+	    }
         } catch (IOException e) {
             System.err.println("Problem when loading HelloWorld: " + e);
             e.printStackTrace();
