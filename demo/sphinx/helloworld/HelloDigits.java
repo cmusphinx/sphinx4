5170bdaa3184fe459c1db35176fe12ddabd8f045
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2004-04-16 20:54:28 +0000

    Added version of the HelloWorld that does not use the endpointer. You will say one utterance, and when you finish with the utterance, you press enter.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@3034 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ demo/sphinx/helloworld/HelloDigits.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 1999-2004 Carnegie Mellon University.
+ * Portions Copyright 2004 Sun Microsystems, Inc.
+ * Portions Copyright 2004 Mitsubishi Electronic Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ *
+ */
+
+package demo.sphinx.helloworld;
+
+import edu.cmu.sphinx.frontend.util.Microphone;
+import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
+import edu.cmu.sphinx.util.props.PropertyException;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+
+/**
+ * A simple HelloDigits demo showing a simple speech application 
+ * built using Sphinx-4.
+ */
+public class HelloDigits {
+
+    /**
+     * Main method for running the HelloDigits demo.
+     */
+    public static void main(String[] args) {
+        try {
+            URL url;
+            if (args.length > 0) {
+                url = new File(args[0]).toURI().toURL();
+            } else {
+                url = HelloDigits.class.getResource("helloworld.config.xml");
+            }
+
+            ConfigurationManager cm = new ConfigurationManager(url);
+
+	    Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+	    final Microphone microphone = 
+                (Microphone) cm.lookup("microphone");
+
+            recognizer.allocate();
+
+	    Thread t = new Thread() {
+		    public void run() {
+			try {
+			    while (true) {
+				if (System.in.read() == 10) {
+				    microphone.stopRecording();
+				}
+			    }			    
+			} catch (IOException ioe) {
+			    ioe.printStackTrace();
+			    System.exit(1);
+			}
+		    }
+		};                    
+	    t.start();
+
+	    System.out.println
+		("Say any digit(s): e.g. \"two oh oh four\", " +
+		 "\"three six five\".");
+	    
+            while (true) {
+		if (microphone.startRecording()) {
+                    System.out.println
+			("Start speaking. " +
+			 "Press ENTER when you finish speaking.");
+                    
+		    Result result = recognizer.recognize();
+                    
+                    if (result != null) {
+                        String resultText = result.getBestResultNoFiller();
+                        System.out.println("You said: " + resultText + "\n");
+                    } else {
+                        System.out.println("I can't hear what you said.\n");
+                    }                    
+                } else {
+                    System.out.println("Cannot start microphone.");
+                    recognizer.deallocate();
+                    System.exit(1);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Problem when loading HelloDigits: " + e);
+            e.printStackTrace();
+        } catch (PropertyException e) {
+            System.err.println("Problem configuring HelloDigits: " + e);
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            System.err.println("Problem creating HelloDigits: " + e);
+            e.printStackTrace();
+        }
+    }
+}
