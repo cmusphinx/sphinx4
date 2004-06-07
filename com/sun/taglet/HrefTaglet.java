bfde4cdc1bf7cd0e6b8ea04fa6a74cf5b341e096
wwalker <wwalker@94700074-3cef-4d97-a70e-9c8c206c02f5>
2004-06-07 13:18:23 +0000

    Add HrefTaglet and initial use in overview.html.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@3334 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ com/sun/taglet/HrefTaglet.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 1998-2003 Sun Microsystems, Inc.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ */
+package com.sun.taglet;
+
+import com.sun.javadoc.*;
+
+import com.sun.tools.doclets.*;
+import com.sun.tools.doclets.standard.*;
+import com.sun.tools.doclets.standard.tags.*;
+
+import java.util.*;
+
+public class HrefTaglet extends AbstractInlineTaglet {
+
+    private static final String NAME = "href";
+
+    public HrefTaglet() {
+        name = NAME;
+    }
+
+    public static void register(Map tagletMap) {
+       HrefTaglet tag = new HrefTaglet();
+       Taglet t = (Taglet) tagletMap.get(tag.getName());
+       if (t != null) {
+           tagletMap.remove(tag.getName());
+       }
+       tagletMap.put(tag.getName(), tag);
+    }
+
+
+    public String toString(Tag tag, Doc doc, HtmlStandardWriter writer) {
+        StringBuffer result = new StringBuffer("href=\""+
+            writer.relativepath);
+        ClassDoc classDoc = writer.configuration().root.classNamed(tag.text());
+        if (classDoc == null) {
+            //Handle bad link here.
+        }
+        try {
+            result.append(
+                DirectoryManager.getPathToPackage(
+                    classDoc.containingPackage(),
+                    classDoc.name() + ".html"));
+            result.append("\"");
+            return result.toString();
+        } catch (Exception e) {
+            System.err.println(
+                "@href tag text does not refer to a valid classref: "
+                + tag.text());
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+            "When run with the javadoc tool, the HrefTaglet\n"
+            + "will replace inline references such as\n\n"
+            + "  {@href com.acme.babblefish.BabbleFish}\n\n"
+            + "with a string such as\n\n"
+            + "  com/acme/babblefish/BabbleFish.html\n\n");
+    }
+}
