63e3fc0a39839666ac097600847831ac2f5a4f00
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2004-08-24 13:45:08 +0000

    Throws an error if the bits per sample is not a multiple of 8.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@3746 94700074-3cef-4d97-a70e-9c8c206c02f5

--- edu/cmu/sphinx/frontend/util/StreamDataSource.java
+++ edu/cmu/sphinx/frontend/util/StreamDataSource.java
@@ -109,11 +109,16 @@ public class StreamDataSource extends BaseDataProcessor {
         super.newProperties(ps);
         sampleRate = ps.getInt(PROP_SAMPLE_RATE, PROP_SAMPLE_RATE_DEFAULT);
         bytesPerRead = ps.getInt(PROP_BYTES_PER_READ,
-                PROP_BYTES_PER_READ_DEFAULT);
-        bytesPerValue = ps.getInt(PROP_BITS_PER_SAMPLE,
-                PROP_BITS_PER_SAMPLE_DEFAULT) / 8;
+                                 PROP_BYTES_PER_READ_DEFAULT);
+        int bitsPerSample = ps.getInt(PROP_BITS_PER_SAMPLE,
+                                      PROP_BITS_PER_SAMPLE_DEFAULT);
+        if (bitsPerSample % 8 != 0) {
+            throw new Error("StreamDataSource: bits per sample must be a " +
+                            "multiple of 8.");
+        }
+        bytesPerValue = bitsPerSample / 8;
         bigEndian = ps.getBoolean(PROP_BIG_ENDIAN_DATA,
-                PROP_BIG_ENDIAN_DATA_DEFAULT);
+                                  PROP_BIG_ENDIAN_DATA_DEFAULT);
         signedData = ps.getBoolean(PROP_SIGNED_DATA, PROP_SIGNED_DATA_DEFAULT);
         if (bytesPerRead % 2 == 1) {
             bytesPerRead++;
