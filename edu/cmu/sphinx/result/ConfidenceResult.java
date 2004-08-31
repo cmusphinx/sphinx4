a00ed2d1ebb415411db2a030342b202af4d2a59b
ppk96 <ppk96@94700074-3cef-4d97-a70e-9c8c206c02f5>
2004-08-31 19:03:42 +0000

    Checking in interfaces for the confidence scorer and confidence result.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@3803 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ edu/cmu/sphinx/result/ConfidenceResult.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 1999-2004 Carnegie Mellon University.
+ * Portions Copyright 2004 Sun Microsystems, Inc.
+ * Portions Copyright 2004 Mitsubishi Electric Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ *
+ */
+package edu.cmu.sphinx.result;
+
+import java.util.Iterator;
+
+public interface ConfidenceResult {
+
+    /**
+     * Returns the best hypothesis of the result.
+     *
+     * @return the best hypothesis of the result
+     */
+    public Path getBestHypothesis();
+
+    /**
+     * Get the number of word slots contained in this result
+     *
+     * @return length of the result
+     */
+    public int size();
+
+    /**
+     * Iterator through the confusion sets in this result.
+     *
+     * @return confusion set iterator
+     */
+    public Iterator confusionSetIterator();
+
+    /**
+     * Get the nth confusion set in this result
+     *
+     * @param i the index of the confusion set to get
+     * @return the requested confusion set
+     */
+    public ConfusionSet getConfusionSet(int i); 
+}
---
+++ edu/cmu/sphinx/result/ConfusionSet.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 1999-2004 Carnegie Mellon University.
+ * Portions Copyright 2004 Sun Microsystems, Inc.
+ * Portions Copyright 2004 Mitsubishi Electric Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ *
+ */
+package edu.cmu.sphinx.result;
+
+import java.util.TreeMap;
+
+/**
+ * TODO: fill in a description
+ */ 
+public class ConfusionSet extends TreeMap {
+
+    // TODO: implements
+}
