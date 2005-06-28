ff4ba6f33683e3e90c3a2d30c6665e5cccc69727
sumpfork <sumpfork@94700074-3cef-4d97-a70e-9c8c206c02f5>
2005-06-28 21:31:49 +0000

    correct string distance calculation and make method static
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@4622 94700074-3cef-4d97-a70e-9c8c206c02f5

--- edu/cmu/sphinx/result/SausageMaker.java
+++ edu/cmu/sphinx/result/SausageMaker.java
@@ -156,30 +156,30 @@ public class SausageMaker extends AbstractSausageMaker {
      * @param p2 the second list
      * @return the string edit distance between the two lists
      */
-    protected int stringEditDistance(List p1, List p2) {
+    protected static int stringEditDistance(List p1, List p2) {
         if (p1.size() == 0) {
             return p2.size();
         }
         if (p2.size() == 0) {
             return p1.size();
         }
-        int [][] distances = new int[p1.size()][p2.size()];
-        distances[0][0] = 0;
-        for (int i=0;i<p1.size();i++) {
+        int [][] distances = new int[p1.size()+1][p2.size()+1];
+        for (int i=0;i<=p1.size();i++) {
             distances[i][0] = i;
         }
-        for (int j=0;j<p2.size();j++) {
+        for (int j=0;j<=p2.size();j++) {
             distances[0][j] = j;
         }
-        for (int i=1;i<p1.size();i++) {
-            for (int j=1;j<p2.size();j++) {
+        for (int i=1;i<=p1.size();i++) {
+            for (int j=1;j<=p2.size();j++) {
                 int min = Math.min(distances[i-1][j-1]
-                                       + (p1.get(i).equals(p2.get(j)) ? 0 : 1),
+                                       + (p1.get(i-1).equals(p2.get(j-1)) ? 0 : 1),
                                    distances[i-1][j] + 1);
                 min = Math.min(min,distances[i][j-1] + 1);
-            }            
+                distances[i][j] = min;
+            }
         }
-        return distances[p1.size()-1][p2.size()-1];
+        return distances[p1.size()][p2.size()];
     }
     
     /**
