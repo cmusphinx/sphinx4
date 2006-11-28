85cea86d592b56fcd87ff2eed6594e2eb867ac17
holgerbrandl <holgerbrandl@94700074-3cef-4d97-a70e-9c8c206c02f5>
2006-11-28 17:40:38 +0000

    integrated patch 1601376: equally sized blocks for voice activity detection
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@6349 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ src/sphinx4/edu/cmu/sphinx/frontend/DataBlocker.java
@@ -0,0 +1,118 @@
+package edu.cmu.sphinx.frontend;
+
+import edu.cmu.sphinx.util.props.PropertyException;
+import edu.cmu.sphinx.util.props.PropertySheet;
+import edu.cmu.sphinx.util.props.PropertyType;
+import edu.cmu.sphinx.util.props.Registry;
+
+import java.util.LinkedList;
+
+/**
+ * A <code>DataProcessor</code> which wraps incoming <code>DoubleData</code>-objects into equally size blocks of defined
+ * length.
+ */
+public class DataBlocker extends BaseDataProcessor {
+
+    /**
+     * The SphinxProperty name for the block size of generated data-blocks in milliseconds.
+     */
+    public static final String PROP_BLOCK_SIZE_MS = "blockSizeMs";
+
+    /**
+     * The default value for PROP_BLOCK_SIZE_MS.
+     */
+    public static final float PROP_BLOCK_SIZE_MS_DEFAULT = 10;
+
+    private double blockSizeMs;
+    private int blockSizeSamples = Integer.MAX_VALUE;
+
+    private int curFirstSamplePos;
+    private int sampleRate = -1;
+
+    private LinkedList<DoubleData> inBuffer = new LinkedList<DoubleData>();
+
+    private int curInBufferSize = 0;
+
+
+    public DataBlocker() {
+    }
+
+
+    public DataBlocker(double blockSizeMs) {
+        this.blockSizeMs = blockSizeMs;
+    }
+
+
+    public Data getData() throws DataProcessingException {
+        // collect enough data to be able to create a new data block
+        while (curInBufferSize < blockSizeSamples) {
+            Data data = getPredecessor().getData();
+
+            if (data instanceof DataStartSignal) {
+                sampleRate = ((DataStartSignal) data).getSampleRate();
+                blockSizeSamples = (int) Math.round(sampleRate * blockSizeMs / 1000);
+            }
+
+            if (!(data instanceof DoubleData)) {
+                inBuffer.clear();
+                curInBufferSize = 0;
+                return data;
+            }
+
+            DoubleData dd = (DoubleData) data;
+
+            inBuffer.add(dd);
+            curInBufferSize += dd.getValues().length;
+        }
+
+        // now we are ready to merge all data blocks into one
+        double[] newSampleBlock = new double[blockSizeSamples];
+
+        int copiedSamples = 0;
+
+        long firstSample = inBuffer.get(0).getFirstSampleNumber() + curFirstSamplePos;
+        long collectTime = inBuffer.get(0).getCollectTime();
+
+        while (!inBuffer.isEmpty()) {
+            DoubleData dd = inBuffer.remove(0);
+            double[] values = dd.getValues();
+            int copyLength = Math.min(blockSizeSamples - copiedSamples, values.length - curFirstSamplePos);
+
+            System.arraycopy(values, curFirstSamplePos, newSampleBlock, copiedSamples, copyLength);
+
+            // does the current data-object contains more samples than necessary? -> keep the rest for the next block
+            if (copyLength < (values.length - curFirstSamplePos)) {
+                assert inBuffer.isEmpty();
+
+                curFirstSamplePos += copyLength;
+                inBuffer.add(0, dd);
+                break;
+            } else {
+                copiedSamples += copyLength;
+                curFirstSamplePos = 0;
+            }
+        }
+
+        curInBufferSize = inBuffer.isEmpty() ? 0 : inBuffer.get(0).getValues().length - curFirstSamplePos;
+        return new DoubleData(newSampleBlock, sampleRate, collectTime, firstSample);
+    }
+
+
+    public void register(String string, Registry registry) throws PropertyException {
+        super.register(string, registry);
+
+        registry.register(PROP_BLOCK_SIZE_MS, PropertyType.DOUBLE);
+    }
+
+
+    public void newProperties(PropertySheet propertySheet) throws PropertyException {
+        super.newProperties(propertySheet);
+
+        blockSizeMs = propertySheet.getDouble(PROP_BLOCK_SIZE_MS, PROP_BLOCK_SIZE_MS_DEFAULT);
+    }
+
+
+    public String getName() {
+        return this.getClass().getName();
+    }
+}
---
+++ src/sphinx4/edu/cmu/sphinx/frontend/test/DataBlockerTest.java
@@ -0,0 +1,139 @@
+package edu.cmu.sphinx.frontend.test;
+
+import edu.cmu.sphinx.frontend.*;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Some small tests which ensure that the <code>DataBlocker</code> works properly.
+ */
+public class DataBlockerTest extends BaseDataProcessor {
+
+    private List<Data> input;
+
+
+    @Before
+    public void setUp() {
+        input = new ArrayList<Data>();
+    }
+
+
+    @Test
+    public void testLongInput() throws DataProcessingException {
+        int sampleRate = 1000;
+
+        input.add(new DataStartSignal(sampleRate));
+
+        input.addAll(createDataInput(1000, 1000, sampleRate, 0)); // create one second of data sampled with 1kHz
+        input.add(new DataEndSignal(0));
+
+        assertTrue(hasIncreasingOrder(collectOutput(100), 1000));
+    }
+
+
+    @Test
+    public void testUsualInput() throws DataProcessingException {
+        int sampleRate = 1000;
+
+        input.add(new DataStartSignal(sampleRate));
+
+        input.addAll(createDataInput(600, 120, sampleRate, 0));
+        input.add(new DataEndSignal(0));
+
+        List<Data> output = collectOutput(100);
+
+        assertEquals(output.size(), 6);
+        assertEquals(201, ((DoubleData) output.get(2)).getFirstSampleNumber());
+        assertTrue(hasIncreasingOrder(output, 600));
+    }
+
+
+    @Test
+    public void skipLastSamples() throws DataProcessingException {
+        int sampleRate = 1000;
+        input.add(new DataStartSignal(sampleRate));
+
+        input.addAll(createDataInput(500, 500, sampleRate, 0));
+        input.addAll(createDataInput(300, 300, sampleRate, 500));
+        input.add(new DataEndSignal(0));
+
+        List<Data> output = collectOutput(250);
+
+        assertEquals(output.size(), 3);
+        assertEquals(501, ((DoubleData) output.get(2)).getFirstSampleNumber());
+        assertTrue(hasIncreasingOrder(output, 750));
+    }
+
+
+    /**
+     * Returns the processed Data output.
+     *
+     * @return an Data object that has been processed by this DataProcessor
+     * @throws edu.cmu.sphinx.frontend.DataProcessingException
+     *          if a data processor error occurs
+     */
+    public Data getData() throws DataProcessingException {
+        return input.remove(0);
+    }
+
+
+    private List<Data> collectOutput(double blocSizeMs) throws DataProcessingException {
+        DataBlocker dataBlocker = new DataBlocker(blocSizeMs);
+        dataBlocker.setPredecessor(this);
+
+        List<Data> output = new ArrayList<Data>();
+
+        while (true) {
+            Data d = dataBlocker.getData();
+            if (d instanceof DoubleData)
+                output.add(d);
+            if (d instanceof DataEndSignal)
+                return output;
+        }
+    }
+
+
+    private static List<DoubleData> createDataInput(int numSamples, int blockSize, int sampleRate, int offSet) {
+        List<DoubleData> datas = new ArrayList<DoubleData>();
+
+        double counter = 1;
+        for (int i = 0; i < numSamples / blockSize; i++) {
+            double[] values = new double[blockSize];
+            datas.add(new DoubleData(values, sampleRate, 0, (long) counter + offSet));
+
+            for (int j = 0; j < values.length; j++)
+                values[j] = counter++ + offSet;
+        }
+
+        return datas;
+    }
+
+
+    /**
+     * Tests wether the samples of all <code>Data</code>s in the list are ordered in increasing order with +1
+     * increments.
+     */
+    private static boolean hasIncreasingOrder(List<Data> output, int lastValue) {
+        double dataCounter = 0;
+
+        for (Data data : output) {
+            if (data instanceof DoubleData) {
+                DoubleData dd = (DoubleData) data;
+
+                for (int i = 0; i < dd.getValues().length; i++) {
+                    if ((dataCounter + 1) == dd.getValues()[i])
+                        dataCounter++;
+                    else
+                        return false;
+                }
+            }
+        }
+
+        return dataCounter == lastValue;
+    }
+}
