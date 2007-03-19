511931f86f454e0e305d4ab5052eaf68af46ab91
holgerbrandl <holgerbrandl@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-03-19 09:18:22 +0000

    added import Map and HashMap to make s4 compilable.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@6551 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/sphinx4/edu/cmu/sphinx/frontend/util/WavWriter.java
+++ src/sphinx4/edu/cmu/sphinx/frontend/util/WavWriter.java
@@ -14,6 +14,8 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import java.io.*;
+import java.util.Map;
+import java.util.HashMap;
 
 
 /**
