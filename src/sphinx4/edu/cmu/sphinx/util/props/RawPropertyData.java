375b47a0372a5650982674e344cf4fe979bb4ee3
holgerbrandl <holgerbrandl@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-04-04 09:09:01 +0000

    generified RawPropertyData
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@6568 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/sphinx4/edu/cmu/sphinx/util/props/RawPropertyData.java
+++ src/sphinx4/edu/cmu/sphinx/util/props/RawPropertyData.java
@@ -17,75 +17,74 @@ import java.util.List;
 import java.util.Map;
 
 
-/**
- * Holds the raw property data just as it has come in from the properties
- * file.
- */
+/** Holds the raw property data just as it has come in from the properties file. */
 class RawPropertyData {
+
     private String name;
     private String className;
-    private Map properties;
+    private Map<String, Object> properties;
+
+
     /**
      * Creates a raw property data item
-     * 
-     * @param name
-     *            the name of the item
-     * @param className
-     *            the class name of the item
+     *
+     * @param name      the name of the item
+     * @param className the class name of the item
      */
     RawPropertyData(String name, String className) {
         this.name = name;
         this.className = className;
-        properties = new HashMap();
+        properties = new HashMap<String, Object>();
     }
+
+
     /**
      * Adds a new property
-     * 
-     * @param propName
-     *            the name of the property
-     * @param propValue
-     *            the value of the property
+     *
+     * @param propName  the name of the property
+     * @param propValue the value of the property
      */
     void add(String propName, String propValue) {
         properties.put(propName, propValue);
     }
+
+
     /**
      * Adds a new property
-     * 
-     * @param propName
-     *            the name of the property
-     * @param propValue
-     *            the value of the property
+     *
+     * @param propName  the name of the property
+     * @param propValue the value of the property
      */
     void add(String propName, List propValue) {
         properties.put(propName, propValue);
     }
-    /**
-     * @return Returns the className.
-     */
+
+
+    /** @return Returns the className. */
     String getClassName() {
         return className;
     }
-    /**
-     * @return Returns the name.
-     */
+
+
+    /** @return Returns the name. */
     String getName() {
         return name;
     }
-    /**
-     * @return Returns the properties.
-     */
+
+
+    /** @return Returns the properties. */
     Map getProperties() {
         return properties;
     }
-    
+
+
     /**
-     * Determines if the map already contains an entry
-     * for this property
+     * Determines if the map already contains an entry for this property
+     *
      * @param propName the property of interest
      * @return true if the map already contains this property
      */
-     
+
     boolean contains(String propName) {
         return properties.get(propName) != null;
     }
