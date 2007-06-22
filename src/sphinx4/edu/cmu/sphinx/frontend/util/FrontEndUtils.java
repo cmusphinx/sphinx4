8f22dffd7008387f4078cbca895d3ed23c97c81d
holgerbrandl <holgerbrandl@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-06-22 18:24:46 +0000

    added missing brackets and updated some outdated javadoc sections
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@6765 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/sphinx4/edu/cmu/sphinx/frontend/util/FrontEndUtils.java
+++ src/sphinx4/edu/cmu/sphinx/frontend/util/FrontEndUtils.java
@@ -42,11 +42,10 @@ public class FrontEndUtils {
      * <p/>
      * If both approaches fail, an <code>AssertionError</code> becomes thrown.
      *
-     * @param dp The <code>DataProcessor</code> which predecessors are searched backwards to find an instance of a
-     *           <code>RaisedCosineWindower</code>
-     * @return The window shift size of the found <code>RaisedCosineWindower</code>
-     * @throws AssertionError if there was no <code>RaisedCosineWindower</code> within the predecessor list of
-     *                        <code>dp</code>
+     * @param dataProc The <code>DataProcessor</code> which predecessors are searched backwards to some hints about the
+     *                 used window shift size.
+     * @return The found window shift size
+     * @throws RuntimeException If both approaches fail, an <code>AssertionError</code> becomes thrown.
      */
     public static float getWindowShiftMs(DataProcessor dataProc) {
         DataProcessor dp = dataProc;
@@ -54,31 +53,36 @@ public class FrontEndUtils {
         while (!(dp instanceof RaisedCosineWindower)) {
             dp = dp.getPredecessor();
 
-            if (dp == null)
+            if (dp == null) {
                 break;
+            }
 
-            if (dp instanceof FrontEnd)
+            if (dp instanceof FrontEnd) {
                 dp = ((FrontEnd) dp).getLastDataProcessor();
+            }
         }
 
-        if (dp != null)
+        if (dp != null) {
             return ((RaisedCosineWindower) dp).getWindowShiftInMs();
+        }
 
         dp = dataProc;
         while (!(dp instanceof DataBlocker)) {
             dp = dp.getPredecessor();
 
-            if (dp == null)
+            if (dp == null) {
                 break;
+            }
 
-            if (dp instanceof FrontEnd)
+            if (dp instanceof FrontEnd) {
                 dp = ((FrontEnd) dp).getLastDataProcessor();
+            }
         }
 
-        if (dp != null)
+        if (dp != null) {
             return 0;
+        }
 
-        assert false : "Can not dermine the current shift-size of the given feature frontend.";
-        return -1;
+        throw new RuntimeException("Can not dermine the current shift-size of the given feature frontend.");
     }
 }
