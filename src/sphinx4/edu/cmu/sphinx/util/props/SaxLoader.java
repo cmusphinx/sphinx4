a338bd143d4f9b5104e431eb781d18fce58a14b2
holgerbrandl <holgerbrandl@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-02-03 22:42:29 +0000

    Converted ConfigHandler form inner-class to outer-class (to ease coming refactorings)
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@7738 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/sphinx4/edu/cmu/sphinx/util/props/SaxLoader.java
+++ src/sphinx4/edu/cmu/sphinx/util/props/SaxLoader.java
@@ -12,17 +12,17 @@
  */
 package edu.cmu.sphinx.util.props;
 
-import org.xml.sax.*;
-import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.XMLReader;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /** Loads configuration from an XML file */
@@ -56,7 +56,7 @@ public class SaxLoader {
         try {
             SAXParserFactory factory = SAXParserFactory.newInstance();
             XMLReader xr = factory.newSAXParser().getXMLReader();
-            ConfigHandler handler = new ConfigHandler();
+            ConfigHandler handler = new ConfigHandler(rpdMap, globalProperties);
             xr.setContentHandler(handler);
             xr.setErrorHandler(handler);
             InputStream is = url.openStream();
@@ -72,116 +72,5 @@ public class SaxLoader {
             throw new IOException(e.getMessage());
         }
         return rpdMap;
-    }
-
-
-    /** A SAX XML Handler implementation that builds up the map of raw property data objects */
-    class ConfigHandler extends DefaultHandler {
-
-        RawPropertyData rpd = null;
-        Locator locator;
-        List<String> itemList = null;
-        String itemListName = null;
-        StringBuffer curItem;
-
-
-        /* (non-Javadoc)
-        * @see org.xml.sax.ContentHandler#startElement(java.lang.String, java.lang.String, java.lang.String, org.xml.sax.Attributes)
-        */
-        public void startElement(String uri, String localName, String qName,
-                                 Attributes attributes) throws SAXException {
-            if (qName.equals("config")) {
-                // nothing to do
-            } else if (qName.equals("component")) {
-                String curComponent = attributes.getValue("name");
-                String curType = attributes.getValue("type");
-                if (rpdMap.get(curComponent) != null) {
-                    throw new SAXParseException(
-                            "duplicate definition for " + curComponent, locator);
-                }
-                rpd = new RawPropertyData(curComponent, curType);
-            } else if (qName.equals("property")) {
-                String name = attributes.getValue("name");
-                String value = attributes.getValue("value");
-                if (attributes.getLength() != 2 || name == null
-                        || value == null) {
-                    throw new SAXParseException(
-                            "property element must only have "
-                                    + "'name' and 'value' attributes", locator);
-                }
-                if (rpd == null) {
-                    // we are not in a component so add this to the global
-                    // set of symbols
-//                    String symbolName = "${" + name + "}"; // why should we warp the global props here
-                    String symbolName = name;
-                    globalProperties.setValue(symbolName, value);
-                } else if (rpd.contains(name)) {
-                    throw new SAXParseException("Duplicate property: " + name,
-                            locator);
-                } else {
-                    rpd.add(name, value);
-                }
-            } else if (qName.equals("propertylist")) {
-                itemListName = attributes.getValue("name");
-                if (attributes.getLength() != 1 || itemListName == null) {
-                    throw new SAXParseException("list element must only have "
-                            + "the 'name'  attribute", locator);
-                }
-                itemList = new ArrayList<String>();
-            } else if (qName.equals("item")) {
-                if (attributes.getLength() != 0) {
-                    throw new SAXParseException("unknown 'item' attribute",
-                            locator);
-                }
-                curItem = new StringBuffer();
-            } else {
-                throw new SAXParseException("Unknown element '" + qName + "'",
-                        locator);
-            }
-        }
-
-
-        /* (non-Javadoc)
-        * @see org.xml.sax.ContentHandler#characters(char[], int, int)
-        */
-        public void characters(char ch[], int start, int length)
-                throws SAXParseException {
-            if (curItem != null) {
-                curItem.append(ch, start, length);
-            }
-        }
-
-
-        /* (non-Javadoc)
-        * @see org.xml.sax.ContentHandler#endElement(java.lang.String, java.lang.String, java.lang.String)
-        */
-        public void endElement(String uri, String localName, String qName)
-                throws SAXParseException {
-            if (qName.equals("component")) {
-                rpdMap.put(rpd.getName(), rpd);
-                rpd = null;
-            } else if (qName.equals("property")) {
-                // nothing to do
-            } else if (qName.equals("propertylist")) {
-                if (rpd.contains(itemListName)) {
-                    throw new SAXParseException("Duplicate property: "
-                            + itemListName, locator);
-                } else {
-                    rpd.add(itemListName, itemList);
-                    itemList = null;
-                }
-            } else if (qName.equals("item")) {
-                itemList.add(curItem.toString().trim());
-                curItem = null;
-            }
-        }
-
-
-        /* (non-Javadoc)
-        * @see org.xml.sax.ContentHandler#setDocumentLocator(org.xml.sax.Locator)
-        */
-        public void setDocumentLocator(Locator locator) {
-            this.locator = locator;
-        }
     }
 }
