db1d6d4ae4e57677c639efcf031ceecda8eeff81
holgerbrandl <holgerbrandl@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-04-08 21:52:34 +0000

    fixed bug 1937842: s4 just hangs after start without any visible results; added option (default=false) to ignore speech-activity segments
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@8032 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/sphinx4/edu/cmu/sphinx/decoder/scorer/AbstractScorer.java
+++ src/sphinx4/edu/cmu/sphinx/decoder/scorer/AbstractScorer.java
@@ -7,6 +7,7 @@ import edu.cmu.sphinx.frontend.endpoint.SpeechStartSignal;
 import edu.cmu.sphinx.frontend.util.DataUtil;
 import edu.cmu.sphinx.util.props.PropertyException;
 import edu.cmu.sphinx.util.props.PropertySheet;
+import edu.cmu.sphinx.util.props.S4Boolean;
 import edu.cmu.sphinx.util.props.S4Component;
 
 import java.io.IOException;
@@ -25,6 +26,14 @@ public abstract class AbstractScorer implements AcousticScorer {
     @S4Component(type = BaseDataProcessor.class)
     public final static String PROP_FRONTEND = "frontend";
 
+    /**
+     * If set only the feature frames which appear between a <code>SpeechStartSignal</code> and a
+     * <code>SpeechEndSignal</code> will be used for decoding. If not set all feature frames will be used for decoding.
+     */
+    @S4Boolean(defaultValue = true)
+    public static final String USE_STREAM_SIGNALS = "useStreamSignals";
+    private boolean useStreamSignals;
+
     protected BaseDataProcessor frontEnd;
 
     protected Logger logger;
@@ -33,6 +42,8 @@ public abstract class AbstractScorer implements AcousticScorer {
 
     public void newProperties(PropertySheet ps) throws PropertyException {
         frontEnd = (BaseDataProcessor) ps.getComponent(PROP_FRONTEND);
+        useStreamSignals = ps.getBoolean(USE_STREAM_SIGNALS);
+
         logger = ps.getLogger();
         name = ps.getInstanceName();
     }
@@ -53,14 +64,14 @@ public abstract class AbstractScorer implements AcousticScorer {
         try {
             Data data = frontEnd.getData();
 
-            while (data instanceof Signal) {
+            while (data instanceof Signal && useStreamSignals) {
                 if (data instanceof SpeechEndSignal)
                     return null;
 
                 data = frontEnd.getData();
             }
 
-            if (data == null)
+            if (data == null || data instanceof DataEndSignal)
                 return null;
 
             if (data instanceof DoubleData)
@@ -88,7 +99,7 @@ public abstract class AbstractScorer implements AcousticScorer {
         // iterate through the feature stream until a SpeechStartSignal is being found
         int debugCounter = 0;
 
-        while (!(frontEnd.getData() instanceof SpeechStartSignal)) {
+        while (!(frontEnd.getData() instanceof SpeechStartSignal) && useStreamSignals) {
             debugCounter++;
 
             if (debugCounter > 100) { // print a warning every second
@@ -97,7 +108,7 @@ public abstract class AbstractScorer implements AcousticScorer {
             }
         }
 
-        // if this line becomes  reached all following feature frames will be part of a speech segment
+        // (if this line becomes reached && useStreamSignals) all following feature frames will be part of a speech segment
     }
 
 
