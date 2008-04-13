da251406d79f06965916629658a3e5b93d010118
holgerbrandl <holgerbrandl@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-04-13 01:25:30 +0000

    removed no longer needed getShiftSizeMs() (because of recently added DataStartSignal-properties)
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@8061 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/sphinx4/edu/cmu/sphinx/frontend/util/FrontEndUtils.java
+++ src/sphinx4/edu/cmu/sphinx/frontend/util/FrontEndUtils.java
@@ -1,9 +1,7 @@
 package edu.cmu.sphinx.frontend.util;
 
-import edu.cmu.sphinx.frontend.DataBlocker;
 import edu.cmu.sphinx.frontend.DataProcessor;
 import edu.cmu.sphinx.frontend.FrontEnd;
-import edu.cmu.sphinx.frontend.window.RaisedCosineWindower;
 
 /**
  * Some little helper methods to ease the handling of frontend-processor chains.
@@ -27,64 +25,5 @@ public class FrontEndUtils {
 
 
         return (T) dp;
-    }
-
-
-    /**
-     * Applies several heuristics in order to determine the shift-size of the fronted a given <code>DataProcessor</code>
-     * belongs to.
-     * <p/>
-     * <p/>
-     * The shift-size is searched using the following procedure <ol> <li> Try to determine the window shift-size used by
-     * the <code>RaisedCosineWindower</code> which precedes the given <code>DataProcessor</code>. <li> If a data-blocker
-     * is found within the precding processor chain but no <code>RaisedCosineWindower</code> 0 is returned. </ol>
-     * <p/>
-     * <p/>
-     * If both approaches fail, an <code>AssertionError</code> becomes thrown.
-     *
-     * @param dataProc The <code>DataProcessor</code> which predecessors are searched backwards to some hints about the
-     *                 used window shift size.
-     * @return The found window shift size
-     * @throws RuntimeException If both approaches fail, an <code>AssertionError</code> becomes thrown.
-     */
-    public static float getWindowShiftMs(DataProcessor dataProc) {
-        DataProcessor dp = dataProc;
-
-        while (!(dp instanceof RaisedCosineWindower)) {
-            dp = dp.getPredecessor();
-
-            if (dp == null) {
-                break;
-            }
-
-            if (dp instanceof FrontEnd) {
-                dp = ((FrontEnd) dp).getLastDataProcessor();
-            }
-        }
-
-        if (dp != null) {
-            return ((RaisedCosineWindower) dp).getWindowShiftInMs();
-        }
-
-        dp = dataProc;
-        while (!(dp instanceof DataBlocker)) {
-            dp = dp.getPredecessor();
-
-            if (dp == null) {
-                break;
-            }
-
-            if (dp instanceof FrontEnd) {
-                dp = ((FrontEnd) dp).getLastDataProcessor();
-            }
-        }
-
-        if (dp != null && dp instanceof DataBlocker) {
-            return (float) ((DataBlocker) dp).getBlockSizeMs();
-        }
-
-        System.err.println("Can not dermine the current shift-size of the given feature frontend. Using default (10ms) ...");
-        return 10;
-//        throw new RuntimeException("Can not dermine the current shift-size of the given feature frontend.");
     }
 }
