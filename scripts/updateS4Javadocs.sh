1e125fb4c234f3e199d0489e214d53d9db70646e
egouvea <egouvea@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-12-13 15:21:38 +0000

    Changed doc update script to use rsync, index.html and javadoc pages will get updated nightly again
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@8867 94700074-3cef-4d97-a70e-9c8c206c02f5

--- scripts/updateS4Javadocs.sh
+++ scripts/updateS4Javadocs.sh
@@ -2,33 +2,19 @@
 
 . ${HOME}/.profile
 
-JAVA_HOME=/lab/speech/java/j2sdk1.4.2
-export JAVA_HOME
-
-PUSH_DEST=/home/groups/c/cm/cmusphinx/htdocs/sphinx4/
-export PUSH_DEST
-
-DOC_DEST=javadoc
-export DOC_DEST
+PUSH_DEST=htdocs/sphinx4
 
 JAVADOC_OUT=javadoc.out
-
-COPY_LIST=scripts/doclist.txt
 
 LIST=cmusphinx-commits@lists.sourceforge.net
 
 cd /tmp
-cvs -d:ext:cvs.sourceforge.net:/cvsroot/cmusphinx co sphinx4
+${HOME}/bin/ssvn sphinx4
 cd sphinx4
+COPY_LIST=`pwd`/scripts/doclist.txt
 ant javadoc
-gtar cf /tmp/sphinx4docs.tar -C ${DOC_DEST} .
-${HOME}/bin/sscp /tmp/sphinx4docs.tar
-${HOME}/bin/sshh tar -mxf sphinx4docs.tar -C ${PUSH_DEST}/javadoc
-for file in `cat $COPY_LIST`; do
-${HOME}/bin/sshh mkdir -p `dirname ${PUSH_DEST}/$file`
-scp $file shell.sourceforge.net:${PUSH_DEST}/$file
-done
-rm /tmp/sphinx4docs.tar
+srsync $PUSH_DEST javadoc
+srsync $PUSH_DEST `cat $COPY_LIST`
 ant -emacs -Daccess=private clean javadoc > ${JAVADOC_OUT}
 if grep -w -i warning ${JAVADOC_OUT}; then mailx -s "Javadoc result" ${LIST} < ${JAVADOC_OUT};
 elif grep -i error ${JAVADOC_OUT}; then mailx -s "Javadoc result" ${LIST} < ${JAVADOC_OUT};
