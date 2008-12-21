cff2d564d0f1c24c82d98c1ea06b2a824f6ed79a
holgerbrandl <holgerbrandl@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-12-21 17:24:52 +0000

    simplified demo code
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@8902 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/apps/edu/cmu/sphinx/demo/hellodigits/HelloDigits.java
+++ src/apps/edu/cmu/sphinx/demo/hellodigits/HelloDigits.java
@@ -16,10 +16,9 @@ import edu.cmu.sphinx.frontend.util.Microphone;
 import edu.cmu.sphinx.recognizer.Recognizer;
 import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
-import edu.cmu.sphinx.util.props.PropertyException;
 
 import java.io.File;
-import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 
@@ -29,60 +28,38 @@ import java.net.URL;
  */
 public class HelloDigits {
 
-    /** Main method for running the HelloDigits demo. */
-    public static void main(String[] args) {
-        try {
-            URL url;
-            if (args.length > 0) {
-                url = new File(args[0]).toURI().toURL();
+    public static void main(String[] args) throws MalformedURLException {
+        URL url;
+        if (args.length > 0) {
+            url = new File(args[0]).toURI().toURL();
+        } else {
+            url = HelloDigits.class.getResource("hellodigits.config.xml");
+        }
+
+        ConfigurationManager cm = new ConfigurationManager(url);
+
+        // allocate the recognizer
+        Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+        recognizer.allocate();
+
+        // start the microphone
+        Microphone microphone = (Microphone) cm.lookup("microphone");
+        microphone.startRecording();
+
+        System.out.println("Say any digit(s): e.g. \"two oh oh four\" , or \"three six five\".");
+
+        // loop the recognition until the programm exits.
+        while (true) {
+            System.out.println("Start speaking. Press Ctrl-C to quit.\n");
+
+            Result result = recognizer.recognize();
+
+            if (result != null) {
+                String resultText = result.getBestResultNoFiller();
+                System.out.println("You said: " + resultText + "\n");
             } else {
-                url = HelloDigits.class.getResource("hellodigits.config.xml");
+                System.out.println("I can't hear what you said.\n");
             }
-
-            ConfigurationManager cm = new ConfigurationManager(url);
-
-            Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
-            Microphone microphone = (Microphone) cm.lookup("microphone");
-
-            /* allocate the resource necessary for the recognizer */
-            recognizer.allocate();
-
-            /* the microphone will keep recording until the program exits */
-            if (microphone.startRecording()) {
-
-                System.out.println
-                        ("Say any digit(s): e.g. \"two oh oh four\", " +
-                                "\"three six five\".");
-
-                while (true) {
-                    System.out.println
-                            ("Start speaking. Press Ctrl-C to quit.\n");
-
-                    /*
-                     * This method will return when the end of speech
-                     * is reached. Note that the endpointer will determine
-                     * the end of speech.
-                     */
-                    Result result = recognizer.recognize();
-
-                    if (result != null) {
-                        String resultText = result.getBestResultNoFiller();
-                        System.out.println("You said: " + resultText + "\n");
-                    } else {
-                        System.out.println("I can't hear what you said.\n");
-                    }
-                }
-            } else {
-                System.out.println("Cannot start microphone.");
-                recognizer.deallocate();
-                System.exit(1);
-            }
-        } catch (IOException e) {
-            System.err.println("Problem when loading HelloDigits: " + e);
-            e.printStackTrace();
-        } catch (PropertyException e) {
-            System.err.println("Problem configuring HelloDigits: " + e);
-            e.printStackTrace();
         }
     }
 }
