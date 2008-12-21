14fa1be60bf762d8828e33c584dab9b3d087cd25
holgerbrandl <holgerbrandl@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-12-21 17:31:58 +0000

    further simplified some demo-code
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@8903 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/apps/edu/cmu/sphinx/demo/hellongram/HelloNGram.java
+++ src/apps/edu/cmu/sphinx/demo/hellongram/HelloNGram.java
@@ -16,11 +16,6 @@ import edu.cmu.sphinx.frontend.util.Microphone;
 import edu.cmu.sphinx.recognizer.Recognizer;
 import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
-import edu.cmu.sphinx.util.props.PropertyException;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 
 
 /**
@@ -29,74 +24,53 @@ import java.net.URL;
  */
 public class HelloNGram {
 
-    /** Main method for running the HelloNGram demo. */
     public static void main(String[] args) {
-        try {
-            URL url;
-            if (args.length > 0) {
-                url = new File(args[0]).toURI().toURL();
+        ConfigurationManager cm;
+
+        if (args.length > 0) {
+            cm = new ConfigurationManager(args[0]);
+        } else {
+            cm = new ConfigurationManager(HelloNGram.class.getResource("hellongram.config.xml"));
+        }
+
+        // allocate the recognizer
+        System.out.println("Loading...");
+        Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+        recognizer.allocate();
+
+        // start the microphone or exit if the programm if this is not possible
+        Microphone microphone = (Microphone) cm.lookup("microphone");
+        if (!microphone.startRecording()) {
+            System.out.println("Cannot start microphone.");
+            recognizer.deallocate();
+            System.exit(1);
+        }
+
+        printInstructions();
+
+        // loop the recognition until the programm exits.
+        while (true) {
+            System.out.println("Start speaking. Press Ctrl-C to quit.\n");
+
+            Result result = recognizer.recognize();
+
+            if (result != null) {
+                String resultText = result.getBestResultNoFiller();
+                System.out.println("You said: " + resultText + "\n");
             } else {
-                url = HelloNGram.class.getResource("hellongram.config.xml");
+                System.out.println("I can't hear what you said.\n");
             }
-
-            System.out.println("Loading...");
-
-            ConfigurationManager cm = new ConfigurationManager(url);
-
-            Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
-            Microphone microphone = (Microphone) cm.lookup("microphone");
-
-            /* allocate the resource necessary for the recognizer */
-            recognizer.allocate();
-
-            printInstructions();
-
-            /* the microphone will keep recording until the program exits */
-            if (microphone.startRecording()) {
-
-                while (true) {
-                    System.out.println
-                            ("Start speaking. Press Ctrl-C to quit.\n");
-
-                    /*
-                     * This method will return when the end of speech
-                     * is reached. Note that the endpointer will determine
-                     * the end of speech.
-                     */
-                    Result result = recognizer.recognize();
-
-                    if (result != null) {
-                        String resultText = result.getBestResultNoFiller();
-                        System.out.println("You said: " + resultText + "\n");
-                    } else {
-                        System.out.println("I can't hear what you said.\n");
-                    }
-                }
-            } else {
-                System.out.println("Cannot start microphone.");
-                recognizer.deallocate();
-                System.exit(1);
-            }
-        } catch (IOException e) {
-            System.err.println("Problem when loading HelloNGram: " + e);
-            e.printStackTrace();
-        } catch (PropertyException e) {
-            System.err.println("Problem configuring HelloNGram: " + e);
-            e.printStackTrace();
         }
     }
 
 
     /** Prints out what to say for this demo. */
     private static void printInstructions() {
-        System.out.println
-                ("Sample sentences:\n" +
-                        "\n" +
-                        "the green one right in the middle\n" +
-                        "the purple one on the lower right side\n" +
-                        "the closest purple one on the far left side\n" +
-                        "the only one left on the left\n" +
-                        "\n" +
-                        "Refer to the file hellongram.test for a complete list.\n");
+        System.out.println("Sample sentences:\n" +
+                "the green one right in the middle\n" +
+                "the purple one on the lower right side\n" +
+                "the closest purple one on the far left side\n" +
+                "the only one left on the left\n\n" +
+                "Refer to the file hellongram.test for a complete list.\n");
     }
 }
--- src/apps/edu/cmu/sphinx/demo/helloworld/HelloWorld.java
+++ src/apps/edu/cmu/sphinx/demo/helloworld/HelloWorld.java
@@ -16,11 +16,6 @@ import edu.cmu.sphinx.frontend.util.Microphone;
 import edu.cmu.sphinx.recognizer.Recognizer;
 import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
-import edu.cmu.sphinx.util.props.PropertyException;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 
 
 /**
@@ -29,62 +24,40 @@ import java.net.URL;
  */
 public class HelloWorld {
 
-    /** Main method for running the HelloWorld demo. */
     public static void main(String[] args) {
-        try {
-            URL url;
-            if (args.length > 0) {
-                url = new File(args[0]).toURI().toURL();
+        ConfigurationManager cm;
+
+        if (args.length > 0) {
+            cm = new ConfigurationManager(args[0]);
+        } else {
+            cm = new ConfigurationManager(HelloWorld.class.getResource("helloworld.config.xml"));
+        }
+
+        Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+        recognizer.allocate();
+
+        // start the microphone or exit if the programm if this is not possible
+        Microphone microphone = (Microphone) cm.lookup("microphone");
+        if (!microphone.startRecording()) {
+            System.out.println("Cannot start microphone.");
+            recognizer.deallocate();
+            System.exit(1);
+        }
+
+        System.out.println("Say: (Good morning | Hello) ( Bhiksha | Evandro | Paul | Philip | Rita | Will )");
+
+        // loop the recognition until the programm exits.
+        while (true) {
+            System.out.println("Start speaking. Press Ctrl-C to quit.\n");
+
+            Result result = recognizer.recognize();
+
+            if (result != null) {
+                String resultText = result.getBestFinalResultNoFiller();
+                System.out.println("You said: " + resultText + "\n");
             } else {
-                url = HelloWorld.class.getResource("helloworld.config.xml");
+                System.out.println("I can't hear what you said.\n");
             }
-
-            System.out.println("Loading...");
-
-            ConfigurationManager cm = new ConfigurationManager(url);
-
-            Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
-            Microphone microphone = (Microphone) cm.lookup("microphone");
-
-            /* allocate the resource necessary for the recognizer */
-            recognizer.allocate();
-
-            /* the microphone will keep recording until the program exits */
-            if (microphone.startRecording()) {
-
-                System.out.println
-                        ("Say: (Good morning | Hello) " +
-                                "( Bhiksha | Evandro | Paul | Philip | Rita | Will )");
-
-                while (true) {
-                    System.out.println
-                            ("Start speaking. Press Ctrl-C to quit.\n");
-
-                    /*
-                     * This method will return when the end of speech
-                     * is reached. Note that the endpointer will determine
-                     * the end of speech.
-                     */
-                    Result result = recognizer.recognize();
-
-                    if (result != null) {
-                        String resultText = result.getBestFinalResultNoFiller();
-                        System.out.println("You said: " + resultText + "\n");
-                    } else {
-                        System.out.println("I can't hear what you said.\n");
-                    }
-                }
-            } else {
-                System.out.println("Cannot start microphone.");
-                recognizer.deallocate();
-                System.exit(1);
-            }
-        } catch (IOException e) {
-            System.err.println("Problem when loading HelloWorld: " + e);
-            e.printStackTrace();
-        } catch (PropertyException e) {
-            System.err.println("Problem configuring HelloWorld: " + e);
-            e.printStackTrace();
         }
     }
 }
