5552b911db97ec85a19b966d9d8dbe0f61dc5115
egouvea <egouvea@94700074-3cef-4d97-a70e-9c8c206c02f5>
2009-02-04 01:04:12 +0000

    Fixed zipcity script so it copies the right files to the right locations, fixed some links pointing to zipcity; caveat: when I tried to run the demo in my machine, I received a message saying 'unsigned application requesting unrestricted access to system' and the demo does not run
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@8957 94700074-3cef-4d97-a70e-9c8c206c02f5

--- scripts/deployZipCity.sh
+++ scripts/deployZipCity.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-PUSH_DEST=/home/groups/c/cm/cmusphinx/htdocs/sphinx4/
+# Define the destination
+PUSH_DEST=/home/groups/c/cm/cmusphinx/htdocs/sphinx4/src/apps/edu/cmu/sphinx/demo/
 export PUSH_DEST
-cd bld
-tar cvf zipcity.tar zipcity
-sscp zipcity.tar  
-sshh "cd $PUSH_DEST; tar xvf ~/zipcity.tar; rm ~/zipcity.tar;"
-rm zipcity.tar
+# Get the zipcity auxiliary files
+pushd src/apps/edu/cmu/sphinx/demo/zipcity
+bash -x srsync $PUSH_DEST/zipcity *
+popd
+pushd bin
+# Get the zipcity code
+bash -x srsync $PUSH_DEST/zipcity ZipCity.jar
+popd
+pushd lib
+# Get some sphinx4, models
+bash -x srsync $PUSH_DEST/zipcity sphinx4.jar jsapi.jar TIDIGITS_8gau_13dCep_16k_40mel_130Hz_6800Hz.jar
+popd
+
