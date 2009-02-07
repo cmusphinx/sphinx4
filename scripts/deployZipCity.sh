dab92f3e7c241440a0c1341ed1b953211bee33b7
egouvea <egouvea@94700074-3cef-4d97-a70e-9c8c206c02f5>
2009-02-07 18:19:18 +0000

    Fixed zipcity deployment script by signing the jar files
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@8968 94700074-3cef-4d97-a70e-9c8c206c02f5

--- scripts/deployZipCity.sh
+++ scripts/deployZipCity.sh
@@ -2,16 +2,11 @@
 # Define the destination
 PUSH_DEST=/home/groups/c/cm/cmusphinx/htdocs/sphinx4/src/apps/edu/cmu/sphinx/demo/
 export PUSH_DEST
-# Get the zipcity auxiliary files
-pushd src/apps/edu/cmu/sphinx/demo/zipcity
-bash -x srsync $PUSH_DEST/zipcity *
-popd
-pushd bin
-# Get the zipcity code
-bash -x srsync $PUSH_DEST/zipcity ZipCity.jar
-popd
-pushd lib
-# Get some sphinx4, models
-bash -x srsync $PUSH_DEST/zipcity sphinx4.jar jsapi.jar TIDIGITS_8gau_13dCep_16k_40mel_130Hz_6800Hz.jar
+# Add the scripts directory to the path, just in case
+PATH=`pwd`/scripts:$PATH
+export PATH
+ant -f demo.xml release_zipcity
+pushd bld
+srsync $PUSH_DEST/ zipcity
 popd
 
