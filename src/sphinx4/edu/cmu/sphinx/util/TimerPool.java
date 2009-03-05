58e81c1649e344487fbedd390f146eb310812906
holgerbrandl <holgerbrandl@94700074-3cef-4d97-a70e-9c8c206c02f5>
2009-03-05 10:51:35 +0000

    migrated timer-cache to weakly referenced HashMap. Completed migration to new timing framework. :-)
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@9045 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/sphinx4/edu/cmu/sphinx/util/TimerPool.java
+++ src/sphinx4/edu/cmu/sphinx/util/TimerPool.java
@@ -1,49 +1,59 @@
 package edu.cmu.sphinx.util;
 
-import edu.cmu.sphinx.util.props.Configurable;
-
-import java.util.Iterator;
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
 
 /**
- *  Keeps reference to a list of timers.
+ * Keeps reference to a list of timers.
  *
  * @author Holger Brandl
  */
 public class TimerPool {
-    
-    private static WeakHashMap<Configurable, Timer> weakRefTimerPool = new WeakHashMap<Configurable, Timer>();
 
-    private final static Map<String, Timer> timerPool = new LinkedHashMap<String, Timer>();
+    private static final Map<Object, List<Timer>> weakRefTimerPool = new WeakHashMap<Object, List<Timer>>();
 
+
+    // disable the constructor
+    private TimerPool() {
+
+    }
 
     /**
      * Retrieves (or creates) a timer with the given name
      *
      * @param owner
-     *@param timerName the name of the particular timer to retrieve. If the timer does not already exist, it will be
+     * @param timerName the name of the particular timer to retrieve. If the timer does not already exist, it will be
      *                  created  @return the timer.
      */
     public static Timer getTimer(Object owner, String timerName) {
-        Timer timer = null;
-        
-        timer = timerPool.get(timerName);
-        if (timer == null) {
-            timer = new Timer(timerName);
-            timerPool.put(timerName, timer);
+        if (!weakRefTimerPool.containsKey(owner))
+            weakRefTimerPool.put(owner, new ArrayList<Timer>());
+
+        List<Timer> ownerTimers = weakRefTimerPool.get(owner);
+
+        for (Timer timer : ownerTimers) {
+            if (timer.getName().endsWith(timerName))
+                return timer;
         }
-        return timer;
+
+        // there is no timer named 'timerName' yet, so create it
+        Timer requestedTimer = new Timer(timerName);
+        ownerTimers.add(requestedTimer);
+
+        return requestedTimer;
     }
 
 
     /** Dump all timers */
     public static void dumpAll() {
         showTimesShortTitle();
-        for (Iterator<Timer> i = timerPool.values().iterator(); i.hasNext();) {
-            Timer timer = i.next();
-            timer.dump();
+
+        for (Object owner : weakRefTimerPool.keySet()) {
+            for (Timer timer : weakRefTimerPool.get(owner)) {
+                timer.dump();
+            }
         }
     }
 
@@ -68,9 +78,10 @@ public class TimerPool {
 
     /** Resets all timers */
     public static void resetAll() {
-        for (Iterator<Timer> i = timerPool.values().iterator(); i.hasNext();) {
-            Timer timer = i.next();
-            timer.reset();
+        for (Object owner : weakRefTimerPool.keySet()) {
+            for (Timer timer : weakRefTimerPool.get(owner)) {
+                timer.reset();
+            }
         }
     }
 }
