61350a2aa5e777b66932cb707e0412477925095d
holgerbrandl <holgerbrandl@94700074-3cef-4d97-a70e-9c8c206c02f5>
2009-03-17 17:33:58 +0000

    adopted run/compile scripts for jni-demo to ubuntu
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@9063 94700074-3cef-4d97-a70e-9c8c206c02f5

--- scripts/jni/compile.sh
+++ scripts/jni/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JDK=/your/path/here
+JDK=/usr/lib/jvm/java-6-sun/
 
-cc -I$JDK"/include/linux" -L$JDK"/jre/lib/i386/client/" -L$JDK"/jre/lib/i386" -o cTranscriber -ljvm s4JNIExample.c
+cc -I$JDK"/include" -I$JDK"/include/linux" -L$JDK"/jre/lib/i386/client/" -L$JDK"/jre/lib/i386" -o cTranscriber -ljvm s4JNIExample.c
 
