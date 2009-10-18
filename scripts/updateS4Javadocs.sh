86342a59a8c3d1614c793c835b20a3f087e707bc
egouvea <egouvea@94700074-3cef-4d97-a70e-9c8c206c02f5>
2009-10-18 15:27:54 +0000

    Made updateS4Javadocs.sh less dependent on manual contents of scripts/doclist.txt
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@9377 94700074-3cef-4d97-a70e-9c8c206c02f5

--- scripts/updateS4Javadocs.sh
+++ scripts/updateS4Javadocs.sh
@@ -14,7 +14,7 @@ cd sphinx4
 COPY_LIST=`pwd`/scripts/doclist.txt
 ant javadoc
 srsync $PUSH_DEST javadoc
-srsync $PUSH_DEST `cat $COPY_LIST`
+srsync $PUSH_DEST `cat $COPY_LIST` `find src/apps -name '*html'`
 ant -emacs -Daccess=private clean javadoc > ${JAVADOC_OUT}
 if grep -w -i warning ${JAVADOC_OUT}; then mailx -s "Javadoc result" ${LIST} < ${JAVADOC_OUT};
 elif grep -i error ${JAVADOC_OUT}; then mailx -s "Javadoc result" ${LIST} < ${JAVADOC_OUT};
