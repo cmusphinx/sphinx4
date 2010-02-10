c5b73d4dcc4ab555ccfe8081bc2a32a17d895f2f
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2010-02-10 00:31:14 +0000

    Removes duplicated component
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@9687 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/sphinx4/edu/cmu/sphinx/frontend/filter/Dither.java
+++ src/sphinx4/edu/cmu/sphinx/frontend/filter/Dither.java
@@ -1,5 +1,7 @@
 /*
- * Copyright 2008 Nickolay V. Shmyrev <nshmyrev@yandex.ru>
+ * Copyright 1999-2004 Carnegie Mellon University.  
+ * Portions Copyright 2002-2004 Sun Microsystems, Inc.  
+ * Portions Copyright 2002-2004 Mitsubishi Electric Research Laboratories.
  * All Rights Reserved.  Use is subject to license terms.
  * 
  * See the file "license.terms" for information on usage and
@@ -7,12 +9,18 @@
  * WARRANTIES.
  *
  */
-package edu.cmu.sphinx.frontend.filter;
+
+package edu.cmu.sphinx.frontend.util;
 
 import edu.cmu.sphinx.frontend.*;
-import edu.cmu.sphinx.util.props.*;
+import edu.cmu.sphinx.util.props.PropertyException;
+import edu.cmu.sphinx.util.props.PropertySheet;
+import edu.cmu.sphinx.util.props.S4Boolean;
+import edu.cmu.sphinx.util.props.S4Double;
 
-import java.util.*;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import java.util.Random;
 
 /**
  * Implements a dither for the incoming packet. The small random noise is added
@@ -22,58 +30,110 @@ import java.util.*;
  */
 public class Dither extends BaseDataProcessor {
 
-	final Random random;
+    /** The maximal value which could be added/substracted to/from the signal*/
+    @S4Double(defaultValue = 2.0)
+    public static final String PROP_MAX_DITHER = "maxDither";
+    private double ditherMax;
 
-    public Dither() {
+    /** The maximal value of dithered values. */
+    @S4Double(defaultValue = Double.MAX_VALUE)
+    public static final String PROP_MAX_VAL = "upperValueBound";
+    private double maxValue;
+
+    /** The minimal value of dithered values. */
+    @S4Double(defaultValue = -Double.MAX_VALUE)
+    public static final String PROP_MIN_VAL = "lowerValueBound";
+    private double minValue;
+
+
+    /** The property about using random seed or not */
+    @S4Boolean(defaultValue = false)
+    public static final String PROP_USE_RANDSEED = "useRandSeed";
+    private boolean useRandSeed;
+
+    public Dither(  double ditherMax, boolean useRandSeed, double maxValue, double minValue) {
         initLogger();
-        this.random = new Random(0);
+
+        this.ditherMax = ditherMax;
+        this.useRandSeed = useRandSeed;
+
+        this.maxValue = maxValue;
+        this.minValue = minValue;
     }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * edu.cmu.sphinx.util.props.Configurable#newProperties(edu.cmu.sphinx.util
-	 * .props.PropertySheet)
-	 */
-	@Override
+    public Dither( ) {
+
+    }
+
+    @Override
     public void newProperties(PropertySheet ps) throws PropertyException {
-		super.newProperties(ps);
-	}
+        super.newProperties(ps);
 
-	/**
-	 * Returns the next Data object being processed by this Dither, or if it is
-	 * a Signal, it is returned without modification.
-	 * 
-	 * @return the next available Data object, returns null if no Data object is
-	 *         available
-	 * @throws DataProcessingException
-	 *             if there is a processing error
-	 * @see Data
-	 */
-	@Override
+        ditherMax = ps.getDouble(PROP_MAX_DITHER);
+        useRandSeed = ps.getBoolean(PROP_USE_RANDSEED);
+
+        maxValue = ps.getDouble(PROP_MAX_VAL);
+        minValue = ps.getDouble(PROP_MIN_VAL);
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+
+
+    /**
+     * Returns the next DoubleData object, which is a dithered version of the input
+     *
+     * @return the next available DoubleData object, or null if no Data is available
+     * @throws edu.cmu.sphinx.frontend.DataProcessingException
+     *          if a data processing error occurred
+     */
+    @Override
     public Data getData() throws DataProcessingException {
-		Data input = getPredecessor().getData();
-		getTimer().start();
-		if (input != null && input instanceof DoubleData) {
-			applyDither(((DoubleData) input).getValues());
-		}
-		getTimer().stop();
-		return input;
-	}
+        Data input = getPredecessor().getData(); // get the spectrum
+        getTimer().start();
+        if (input != null && ditherMax != 0) {
+            if (input instanceof DoubleData || input instanceof FloatData) {
+                input = process(input);
+            }
+        }
 
-	/**
-	 * Applies dither to the given Audio. The preemphasis is applied in place.
-	 * 
-	 * @param in
-	 *            audio data
-	 */
-	private void applyDither(double[] in) {
+        getTimer().stop();
+        return input;
+    }
 
-		for (int i = 0; i < in.length; i++) {
-			if (random.nextBoolean()) {
-				in[i] = in[i] + 1;
-			}
-		}
-	}
+
+    /**
+     * Process data, adding dither
+     *
+     * @param input a frame
+     * @return processed frame
+     * @throws IllegalArgumentException
+     */
+    private DoubleData process(Data input) throws IllegalArgumentException {
+        DoubleData output;
+        Random r;
+        if (useRandSeed)
+            r = new Random();
+        else
+            r = new Random(12345);
+
+        assert input instanceof DoubleData;
+        double[] inFeatures;
+
+        DoubleData doubleData = (DoubleData) input;
+        inFeatures = doubleData.getValues();
+        double[] outFeatures = new double[inFeatures.length];
+        for (int i = 0; i < inFeatures.length; ++i) {
+            outFeatures[i] = r.nextFloat() * 2 * ditherMax - ditherMax + inFeatures[i];
+            outFeatures[i] = max(min(outFeatures[i], maxValue), minValue);
+        }
+
+        output = new DoubleData(outFeatures, doubleData.getSampleRate(),
+                doubleData.getCollectTime(),
+                doubleData.getFirstSampleNumber());
+
+        return output;
+    }
 }
