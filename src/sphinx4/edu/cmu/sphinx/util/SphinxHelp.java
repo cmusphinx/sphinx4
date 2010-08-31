0f3c720360a81b4260a9abe08bf667a96ce02b04
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2010-08-31 15:57:37 +0000

    Preparation before the release
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@10337 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/sphinx4/edu/cmu/sphinx/util/SphinxHelp.java
+++ src/sphinx4/edu/cmu/sphinx/util/SphinxHelp.java
@@ -16,7 +16,7 @@ package edu.cmu.sphinx.util;
 
 public class SphinxHelp {
 
-    private final static String VERSION = "1.0 beta4";
+    private final static String VERSION = "1.0 beta5";
 
     /**
      * The main entry point
