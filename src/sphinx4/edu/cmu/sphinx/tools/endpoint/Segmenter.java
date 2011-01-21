0c072639087059efb0d53dff6dbaf150955524d4
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2011-01-21 21:57:19 +0000

    Trimming option
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@10765 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/sphinx4/edu/cmu/sphinx/tools/endpoint/Segmenter.java
+++ src/sphinx4/edu/cmu/sphinx/tools/endpoint/Segmenter.java
@@ -8,24 +8,30 @@ import edu.cmu.sphinx.frontend.Data;
 import edu.cmu.sphinx.frontend.FrontEnd;
 import edu.cmu.sphinx.frontend.util.AudioFileDataSource;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
+import edu.cmu.sphinx.util.props.ConfigurationManagerUtils;
 
 public class Segmenter {
 
 	public static void main(String[] args) throws MalformedURLException {
 	       URL audioURL;
 
-	        if (args.length == 1) {
+	        if (args.length == 1 || args.length == 2) {
 	            audioURL = new File(args[0]).toURI().toURL();
 	        } else {
-	            System.out.println ("Usage: java -cp lib/batch.jar:lib/sphinx4.jar edu.cmu.sphinx.tools.enpoint.Segmenter <filename>");
+	            System.out.println ("Usage: java -cp lib/batch.jar:lib/sphinx4.jar edu.cmu.sphinx.tools.endpoint.Segmenter <filename> [--no-split]");
 	            return;
 	        }
+	        
 
 	        URL configURL = Segmenter.class.getResource("frontend.config.xml");
 
 	        ConfigurationManager cm = new ConfigurationManager(configURL);
 	        FrontEnd frontend = (FrontEnd) cm.lookup("endpointer");
 
+	        if (args.length >= 2 && args[1].equals("-no-split")) {
+	            ConfigurationManagerUtils.setProperty(cm, "wavWriter", "captureUtterances", "false");
+            }
+	        
 	        frontend.initialize();
 
 	        // configure the audio input for the recognizer
