3b65b10a23ad12ad0ebc814ec470e40f7160c5ab
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-10-01 13:19:36 +0000

    Moved FST grammar test to JUnit
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@11625 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/test/edu/cmu/sphinx/linguist/language/grammar/FSTGrammarTest.java
+++ src/test/edu/cmu/sphinx/linguist/language/grammar/FSTGrammarTest.java
@@ -5,23 +5,24 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.net.URL;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import edu.cmu.sphinx.linguist.acoustic.UnitManager;
 import edu.cmu.sphinx.linguist.dictionary.Dictionary;
 import edu.cmu.sphinx.linguist.dictionary.FastDictionary;
-import edu.cmu.sphinx.linguist.language.grammar.BatchForcedAlignerGrammar;
+import edu.cmu.sphinx.util.LogMath;
 
-public class BatchForcedAlignerGrammarTest {
+public class FSTGrammarTest {
 
     @Test
     public void testForcedAlignerGrammar() throws IOException {
+    	LogMath logMath = new LogMath(1.0001f, true);
         Dictionary dictionary = new FastDictionary(new URL("file:models/acoustic/wsj/dict/digits.dict"), new URL(
                 "file:models/acoustic/wsj/noisedict"), null, false, null, false, false, new UnitManager());
-        BatchForcedAlignerGrammar grammar = new BatchForcedAlignerGrammar(
-                "src/test/edu/cmu/sphinx/linguist/language/grammar/BatchForcedAlignerGrammarTest.utts", true, true, true, true, dictionary);
+        FSTGrammar grammar = new FSTGrammar
+        		("src/test/edu/cmu/sphinx/linguist/language/grammar/FSTGrammarTest.gram", logMath, true, true, true, true, dictionary);
         grammar.allocate();
-        String sentence = grammar.getRandomSentence();
-        assertTrue(sentence.equals("one") || sentence.equals("two") || sentence.equals("three"));
+        Assert.assertEquals(grammar.getGrammarNodes().size(), 9);
     }
 }
