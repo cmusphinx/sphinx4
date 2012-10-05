1313b76b531be12b59db41406d2a95c1abc867b0
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-10-05 13:51:10 +0000

    Unit test for large ngram models
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@11630 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ src/test/edu/cmu/sphinx/linguist/language/ngram/large/test/LargeNgramTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 1999-2012 Carnegie Mellon University.  
+ * Portions Copyright 2002 Sun Microsystems, Inc.  
+ * Portions Copyright 2002 Mitsubishi Electric Research Laboratories.
+ * Portions Copyright 2012 Nexiwave
+ * 
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+package edu.cmu.sphinx.linguist.language.ngram.large.test;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.cmu.sphinx.linguist.WordSequence;
+import edu.cmu.sphinx.linguist.acoustic.UnitManager;
+import edu.cmu.sphinx.linguist.dictionary.Dictionary;
+import edu.cmu.sphinx.linguist.dictionary.FullDictionary;
+import edu.cmu.sphinx.linguist.dictionary.Word;
+import edu.cmu.sphinx.linguist.language.ngram.large.LargeTrigramModel;
+import edu.cmu.sphinx.util.LogMath;
+
+public class LargeNgramTest {
+	@Test
+	public void testNgram() throws IOException {
+		LogMath logMath = new LogMath(1.001f, true);
+		Dictionary dictionary = new FullDictionary(new URL(
+				"file:src/test/edu/cmu/sphinx/linguist/language/ngram/large/test/100.dict"), new URL(
+				"file:models/acoustic/wsj/noisedict"), null, false, null,
+				false, false, new UnitManager());		
+		LargeTrigramModel model = new LargeTrigramModel("", new URL("file:src/test/edu/cmu/sphinx/linguist/language/ngram/large/test/100.arpa.dmp"), null, 100, 100, false, 3, logMath, dictionary, false, 1.0f, 1.0f, 1.0f, false);
+		dictionary.allocate();
+		model.allocate();
+		Assert.assertEquals(3, model.getMaxDepth());
+		
+		Word[] words = {new Word("huggins",  null, false), new Word("daines",  null, false)};
+		Assert.assertEquals(model.getProbability(new WordSequence(words)), -83.161f, 0.001f);
+
+		Word[] words1 = {new Word("huggins",  null, false), new Word("daines",  null, false), new Word("david",  null, false)};
+		Assert.assertEquals(model.getProbability(new WordSequence(words1)), -6766.4487f, 0.001f);
+	}
+}
