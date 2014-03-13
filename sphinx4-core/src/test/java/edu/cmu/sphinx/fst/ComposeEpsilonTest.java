d947fa292d3862e41629332d9bc6290b1bd1e662
mbait <mbait@94700074-3cef-4d97-a70e-9c8c206c02f5>
2014-03-13 00:56:59 +0000

    Fix resource paths.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@12341 94700074-3cef-4d97-a70e-9c8c206c02f5

--- sphinx4-core/src/test/java/edu/cmu/sphinx/fst/ComposeEpsilonTest.java
+++ sphinx4-core/src/test/java/edu/cmu/sphinx/fst/ComposeEpsilonTest.java
@@ -1,44 +1,49 @@
 /**
  * 
- * Copyright 1999-2012 Carnegie Mellon University.  
- * Portions Copyright 2002 Sun Microsystems, Inc.  
- * Portions Copyright 2002 Mitsubishi Electric Research Laboratories.
- * All Rights Reserved.  Use is subject to license terms.
+ * Copyright 1999-2012 Carnegie Mellon University. Portions Copyright 2002 Sun
+ * Microsystems, Inc. Portions Copyright 2002 Mitsubishi Electric Research
+ * Laboratories. All Rights Reserved. Use is subject to license terms.
  * 
- * See the file "license.terms" for information on usage and
- * redistribution of this file, and for a DISCLAIMER OF ALL 
- * WARRANTIES.
- *
+ * See the file "license.terms" for information on usage and redistribution of
+ * this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ * 
  */
 
 package edu.cmu.sphinx.fst;
 
+import static com.google.common.io.Resources.getResource;
+import static edu.cmu.sphinx.fst.Convert.importFst;
+import static edu.cmu.sphinx.fst.operations.Compose.get;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.io.File;
+import java.net.URL;
+
 import org.testng.annotations.Test;
 
-import edu.cmu.sphinx.fst.Fst;
-import edu.cmu.sphinx.fst.operations.Compose;
 import edu.cmu.sphinx.fst.semiring.TropicalSemiring;
+
 
 /**
  * @author John Salatas <jsalatas@users.sourceforge.net>
  * 
  */
-public class ComposeEpsilonTest  {
-    
+public class ComposeEpsilonTest {
+
     @Test
     public void testCompose() {
-        Fst fstA = Convert.importFst("src/test/edu/cmu/sphinx/fst/data/tests/algorithms/composeeps/A",
-                new TropicalSemiring());
-        Fst fstB = Convert.importFst("src/test/edu/cmu/sphinx/fst/data/tests/algorithms/composeeps/B",
-                new TropicalSemiring());
-        Fst fstC = Convert.importFst(
-                "src/test/edu/cmu/sphinx/fst/data/tests/algorithms/composeeps/fstcomposeeps",
-                new TropicalSemiring());
+        URL url = getResource(getClass(), "algorithms/composeeps/A.fst.txt");
+        File parent = new File(url.getPath()).getParentFile();
 
-        Fst fstComposed = Compose.get(fstA, fstB, new TropicalSemiring());
+        String path = new File(parent, "A").getPath();
+        Fst fstA = importFst(path, new TropicalSemiring());
+        path = new File(parent, "B").getPath();
+        Fst fstB = importFst(path, new TropicalSemiring());
+        path = new File(parent, "fstcomposeeps").getPath();
+        Fst fstC = importFst(path, new TropicalSemiring());
+
+        Fst fstComposed = get(fstA, fstB, new TropicalSemiring());
         assertThat(fstC, equalTo(fstComposed));
     }
 
