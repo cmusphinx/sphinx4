d081c4bff536c0a719e21e30a8ef833777329aa8
bogdan petcu <bogdanconstantinpetcu@gmail.com>
2014-07-17 02:42:32 +0300

    code styling improvements + javadocs

--- sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/ReaderDemo.java
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/ReaderDemo.java
@@ -4,31 +4,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Checks basic utility of CountsReader class
+ */
 public class ReaderDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 				System.in));
 		System.out.print("Please enter filepath: ");
 		String fp = null;
+
 		try {
 			fp = reader.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		CountsReader cd = new CountsReader(fp);
-		cd.read();
-		System.out.println(cd.getHeader() + "\n");
-		System.out.println("n_cb = " + cd.getN_cb() + "\n");
-		System.out.println("n_feat = " + cd.getN_feat() + "\n");
-		System.out.println("n_density = " + cd.getN_density() + "\n");
+		CountsReader cd;
+		int nCb, nFeat, nDensity;
+		int[] veclen;
+		float[][][] mean, variance, dnom;
 
-		int[] veclen = cd.getVeclen();
-		float[][][] wt_mean, wt_var, dnom;
-		wt_mean = cd.getWt_mean();
-		wt_var = cd.getWt_var();
+		cd = new CountsReader(fp);
+		cd.read();
+		nCb = cd.getnCb();
+		nFeat = cd.getnFeat();
+		nDensity = cd.getnDensity();
+
+		System.out.println(cd.getHeader() + "\n");
+		System.out.println("n_cb = " + nCb + "\n");
+		System.out.println("n_feat = " + nFeat + "\n");
+		System.out.println("n_density = " + nDensity + "\n");
+
+		veclen = cd.getVeclen();
+		mean = cd.getMean();
+		variance = cd.getVariance();
 		dnom = cd.getDnom();
+
+		System.out.println(mean[0][0][0]);
+		System.out.println(mean[nCb - 1][nFeat - 1][nDensity - 1]);
+		System.out.println(variance[0][0][0]);
+		System.out.println(variance[nCb - 1][nFeat - 1][nDensity - 1]);
+		System.out.println(dnom[0][0][0]);
+		System.out.println(dnom[nCb - 1][nFeat - 1][nDensity - 1]);
 
 	}
 
