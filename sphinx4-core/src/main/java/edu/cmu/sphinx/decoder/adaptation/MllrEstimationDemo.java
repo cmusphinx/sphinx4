f79e8cc548793248bb7d7b6e319a4696942e9a6f
bogdan petcu <bogdanconstantinpetcu@gmail.com>
2014-07-19 22:20:11 +0300

    Fixed dimension problem on pool & updated demo

--- sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/MllrEstimationDemo.java
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/MllrEstimationDemo.java
@@ -11,29 +11,29 @@ public class MllrEstimationDemo {
 		me1.setOutputFilePath("/home/bogdanpetcu/RSoC/data/BillGates_2010/adapted/mllrMatrix");
 		me1.estimateMatrices();
 	
-//		MllrEstimation me2 = new MllrEstimation();
-//
-//		me2.setLocation("/home/bogdanpetcu/RSoC/data/EricMead_2009/adapted/hub4wsj_sc_8k");
-//		me2.setModel("hub4wsj_sc_8k");
-//		me2.setCountsFilePath("/home/bogdanpetcu/RSoC/data/EricMead_2009/unadapted/gauden_counts");
-//		me2.setOutputFileName("mllrMatrix");
-//		me2.estimateMatrices();
-//
-//		MllrEstimation me3 = new MllrEstimation();
-//
-//		me3.setLocation("/home/bogdanpetcu/RSoC/data/GaryFlake_2010/adapted/hub4wsj_sc_8k");
-//		me3.setModel("hub4wsj_sc_8k");
-//		me3.setCountsFilePath("/home/bogdanpetcu/RSoC/data/GaryFlake_2010/unadapted/gauden_counts");
-//		me3.setOutputFileName("mllrMatrix");
-//		me3.estimateMatrices();
-//
-//		MllrEstimation me4 = new MllrEstimation();
-//
-//		me4.setLocation("/home/bogdanpetcu/RSoC/data/JaneMcGonigal_2010/adapted/hub4wsj_sc_8k");
-//		me4.setModel("hub4wsj_sc_8k");
-//		me4.setCountsFilePath("/home/bogdanpetcu/RSoC/data/JaneMcGonigal_2010/unadapted/gauden_counts");
-//		me4.setOutputFileName("mllrMatrix");
-//		me4.estimateMatrices();
+		MllrEstimation me2 = new MllrEstimation();
+
+		me2.setLocation("/home/bogdanpetcu/RSoC/en-us");
+		me2.setModel("en-us");
+		me2.setCountsFilePath("/home/bogdanpetcu/RSoC/data/EricMead_2009/unadapted/gauden_counts");
+		me2.setOutputFilePath("/home/bogdanpetcu/RSoC/data/EricMead_2009/adapted/mllrMatrix");
+		me2.estimateMatrices();
+		
+		MllrEstimation me3 = new MllrEstimation();
+
+		me3.setLocation("/home/bogdanpetcu/RSoC/en-us");
+		me3.setModel("en-us");
+		me3.setCountsFilePath("/home/bogdanpetcu/RSoC/data/GaryFlake_2010/unadapted/gauden_counts");
+		me3.setOutputFilePath("/home/bogdanpetcu/RSoC/data/GaryFlake_2010/adapted/mllrMatrix");
+		me3.estimateMatrices();
+
+		MllrEstimation me4 = new MllrEstimation();
+
+		me4.setLocation("/home/bogdanpetcu/RSoC/en-us");
+		me4.setModel("en-us");
+		me4.setCountsFilePath("/home/bogdanpetcu/RSoC/data/JaneMcGonigal_2010/unadaped/gauden_counts");
+		me4.setOutputFilePath("/home/bogdanpetcu/RSoC/data/JaneMcGonigal_2010/adapted/mllrMatrix");
+		me4.estimateMatrices();
 
 	
 	}
