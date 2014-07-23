00805bef4d64656051c1fb97ef4de31b76d01d61
bogdan petcu <bogdanconstantinpetcu@gmail.com>
2014-07-24 01:07:00 +0300

    implemente CountsCollector class that gathers counts from a result object

--- sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/CountsCollector.java
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/CountsCollector.java
@@ -1,14 +1,103 @@
 package edu.cmu.sphinx.decoder.adaptation;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.cmu.sphinx.decoder.search.Token;
+import edu.cmu.sphinx.frontend.Data;
+import edu.cmu.sphinx.frontend.DoubleData;
+import edu.cmu.sphinx.frontend.FloatData;
+import edu.cmu.sphinx.linguist.SearchState;
+import edu.cmu.sphinx.linguist.acoustic.HMMState;
+import edu.cmu.sphinx.linguist.acoustic.tiedstate.MixtureComponent;
+import edu.cmu.sphinx.result.Result;
+
 public class CountsCollector {
-	
+
 	private Counts counts;
+	private Result result;
+	private int[] vectorLength;
+	private int numStates;
+	private int numStreams;
+	private int numGaussiansPerState;
+	private List<DoubleData> features;
+
+	public CountsCollector(int[] vectorLength, int numStates, int numStreams,
+			int numGaussiansPerState, Result result) {
+		super();
+		this.vectorLength = vectorLength;
+		this.numStates = numStates;
+		this.numStreams = numStreams;
+		this.numGaussiansPerState = numGaussiansPerState;
+		this.result = result;
+	}
 
 	public Counts getCounts() {
 		return counts;
 	}
 
-	public void collect(){
-		
+	public float computeTotalScore(float[] scores) {
+		float totalScore = 0;
+
+		for (int i = 0; i < scores.length; i++) {
+			totalScore += scores[i];
+		}
+
+		return totalScore;
+	}
+
+	public float[] calculateComponentScore(DoubleData feature, HMMState state) {
+		MixtureComponent[] mc = state.getMixtureComponents();
+		float[] mw = state.getLogMixtureWeights();
+		float[] featureVector = FloatData.toFloatData(feature).getValues();
+		float[] logComponentScore = new float[mc.length];
+
+		for (int i = 0; i < mc.length; i++) {
+			logComponentScore[i] = mc[i].getScore(featureVector) + mw[i];
+		}
+
+		return logComponentScore;
+	}
+
+	public float computeLmr(HMMState searchState) {
+		searchState.getState();
+		return 0;
+	}
+
+	public void collect() throws Exception {
+		Token token = result.getBestToken();
+		HMMState state;
+		float lmr;
+		float[] componentScore, featureVector;
+		float totalScore, dn;
+		int mId;
+
+		if (token == null)
+			throw new Exception("Best token not found!");
+
+		float[][][] dnom;
+		float[][][][] means;
+
+		dnom = new float[numStates][numStreams][numGaussiansPerState];
+		means = new float[numStates][numStreams][numGaussiansPerState][vectorLength[0]];
+
+		do {
+			DoubleData feature = (DoubleData) token.getData();
+			state = (HMMState) token.getSearchState();
+			componentScore = this.calculateComponentScore(feature, state);
+			totalScore = this.computeTotalScore(componentScore);
+			featureVector = FloatData.toFloatData(feature).getValues();
+			mId = (int) state.getMixtureId();
+
+			for (int i = 0; i < componentScore.length; i++) {
+				dn = dnom[mId][0][i] = componentScore[i] / totalScore;
+				for (int j = 0; j < featureVector.length; j++) {
+					means[mId][0][i][j] = dn * featureVector[j];
+				}
+			}
+
+			token = token.getPredecessor();
+		} while (token != null);
+
 	}
 }
