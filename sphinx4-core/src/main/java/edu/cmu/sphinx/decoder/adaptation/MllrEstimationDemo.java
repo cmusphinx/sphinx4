a73a334db71cbaf4a3df507750a23f9bc6e0b097
bogdan petcu <bogdanconstantinpetcu@gmail.com>
2014-07-31 22:18:30 +0300

    added modifications so that collecting counts to be done for each read Result object as it is read, not for all after reading them

--- sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/MllrEstimationDemo.java
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/MllrEstimationDemo.java
@@ -10,6 +10,7 @@ public class MllrEstimationDemo {
 		me1.setCountsFilePath("/home/bogdanpetcu/RSoC/data/BillGates_2010/unadapted/gauden_counts");
 		me1.setOutputFilePath("/home/bogdanpetcu/RSoC/data/BillGates_2010/adapted/mllrMatrix");
 		me1.setCountsFromFile(true);
+		me1.init();
 		me1.estimateMatrices();
 		me1.createMllrFile();
 	
@@ -20,6 +21,7 @@ public class MllrEstimationDemo {
 		me2.setCountsFilePath("/home/bogdanpetcu/RSoC/data/EricMead_2009/unadapted/gauden_counts");
 		me2.setOutputFilePath("/home/bogdanpetcu/RSoC/data/EricMead_2009/adapted/mllrMatrix");
 		me2.setCountsFromFile(true);
+		me2.init();
 		me2.estimateMatrices();
 		me2.createMllrFile();
 		
@@ -30,6 +32,7 @@ public class MllrEstimationDemo {
 		me3.setCountsFilePath("/home/bogdanpetcu/RSoC/data/GaryFlake_2010/unadapted/gauden_counts");
 		me3.setOutputFilePath("/home/bogdanpetcu/RSoC/data/GaryFlake_2010/adapted/mllrMatrix");
 		me3.setCountsFromFile(true);
+		me3.init();
 		me3.estimateMatrices();
 		me3.createMllrFile();
 		
@@ -40,6 +43,7 @@ public class MllrEstimationDemo {
 		me4.setCountsFilePath("/home/bogdanpetcu/RSoC/data/JaneMcGonigal_2010/unadaped/gauden_counts");
 		me4.setOutputFilePath("/home/bogdanpetcu/RSoC/data/JaneMcGonigal_2010/adapted/mllrMatrix");
 		me4.setCountsFromFile(true);
+		me4.init();
 		me4.estimateMatrices();
 		me4.createMllrFile();
 	
