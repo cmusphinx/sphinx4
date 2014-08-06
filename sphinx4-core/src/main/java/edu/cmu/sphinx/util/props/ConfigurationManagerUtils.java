08289f00cc59b3322f486a40f997102e8c7d32e5
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2014-08-06 19:29:52 +0000

    Ensure utf-8 output on serialization
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@12550 94700074-3cef-4d97-a70e-9c8c206c02f5

--- sphinx4-core/src/main/java/edu/cmu/sphinx/util/props/ConfigurationManagerUtils.java
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/util/props/ConfigurationManagerUtils.java
@@ -7,6 +7,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -294,7 +296,7 @@ public final class ConfigurationManagerUtils {
 
         assert cm != null;
         try {
-            PrintWriter pw = new PrintWriter(new FileOutputStream(cmLocation));
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(cmLocation), StandardCharsets.UTF_8));
             String configXML = ConfigurationManagerUtils.toXML(cm);
             pw.print(configXML);
             pw.flush();
