35ab841319c5de2ce86595cac2faadbd11bb6eb7
bogdan petcu <bogdanconstantinpetcu@gmail.com>
2014-08-11 18:59:05 +0300

    added regtree package and implemented kmeans clustering for dividing means in clusters

--- sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/regtree/Tree.java
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/regtree/Tree.java
@@ -1,5 +1,15 @@
 package edu.cmu.sphinx.decoder.adaptation.regtree;
 
 public class Tree {
+	
+	private Node root;
+
+	public void setRoot(Node root) {
+		this.root = root;
+	}
+
+	public Node getRoot() {
+		return root;
+	}
 
 }
