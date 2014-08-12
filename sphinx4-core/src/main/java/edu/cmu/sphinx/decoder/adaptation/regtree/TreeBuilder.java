3b7284effea9d7097ca11a8503a829a060376dc7
bogdan petcu <bogdanconstantinpetcu@gmail.com>
2014-08-12 03:05:49 +0300

    implemented mllr estimations for a given number of clusters

--- sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/regtree/TreeBuilder.java
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/regtree/TreeBuilder.java
@@ -1,38 +1,11 @@
 package edu.cmu.sphinx.decoder.adaptation.regtree;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-
-import edu.cmu.sphinx.api.Configuration;
-import edu.cmu.sphinx.api.StreamSpeechRecognizer;
-
 public class TreeBuilder {
 
 	private Tree tree;
-	
-	public Tree getTree(){
+
+	public Tree getTree() {
 		return this.tree;
 	}
 
-	public static void main(String[] args) throws IOException {
-		Configuration configuration = new Configuration();
-
-		configuration.setAcousticModelPath("/home/bogdanpetcu/RSoC/en-us");
-		configuration
-				.setDictionaryPath("/home/bogdanpetcu/RSoC/wsj/dict/cmudict.0.6d");
-		configuration
-				.setLanguageModelPath("/home/bogdanpetcu/RSoC/sphinx4/sphinx4-data/src/main/resources/edu/cmu/sphinx/models/language/en-us.lm.dmp");
-
-		StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(
-				configuration);
-
-		InputStream stream = TreeBuilder.class
-				.getResourceAsStream("/home/bogdanpetcu/RSoC/adaptation-test/BillGates_2010/out.wav");
-		recognizer.startRecognition(stream, false);
-		
-		Cluster initialMeans = new Cluster(recognizer.getLoader().getMeansPool());
-		ArrayList<Cluster> clusters = initialMeans.kMeansClustering(10, 15);
-		System.out.println(clusters.size());
-	}
 }
