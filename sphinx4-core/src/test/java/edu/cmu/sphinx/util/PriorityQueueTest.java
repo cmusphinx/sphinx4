8ea2826ebca97cc2de05f9faf61103ff0c95e531
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2014-08-21 14:55:05 +0000

    Fix package name
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@12581 94700074-3cef-4d97-a70e-9c8c206c02f5

--- sphinx4-core/src/test/java/edu/cmu/sphinx/util/PriorityQueueTest.java
+++ sphinx4-core/src/test/java/edu/cmu/sphinx/util/PriorityQueueTest.java
@@ -1,4 +1,4 @@
-package edu.cmu.sphinx.common.collections;
+package edu.cmu.sphinx.util;
 
 import static com.google.common.base.Functions.forMap;
 import static com.google.common.collect.Maps.newHashMap;
