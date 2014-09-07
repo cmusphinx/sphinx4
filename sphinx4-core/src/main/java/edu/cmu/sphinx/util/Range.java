7056ac44ba2880037bd3106c25f4bb5419528b6b
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2014-09-07 07:42:28 +0000

    Added missing file
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@12589 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/util/Range.java
@@ -0,0 +1,25 @@
+package edu.cmu.sphinx.util;
+
+public class Range {
+
+    private int start;
+    private int end;
+    
+    public Range(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public boolean contains(int shift) {
+        return shift >= start && shift <= end;
+    }
+
+    public int lowerEndpoint() {
+        return start;
+    }
+
+    public int upperEndpoint() {
+        return end;
+    }
+
+}
