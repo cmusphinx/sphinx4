c4c927d3478ed42c2c1af8c20781796fee611526
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2014-09-28 15:59:21 +0000

    Speaker-adaptive decoding in SpeakerID demo.
    See for details https://github.com/cmusphinx/sphinx4/pull/10
    
    Thanks to Georgiana Chelu
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@12629 94700074-3cef-4d97-a70e-9c8c206c02f5

--- sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/Transform.java
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/decoder/adaptation/Transform.java
@@ -1,6 +1,9 @@
 package edu.cmu.sphinx.decoder.adaptation;
 
+import java.io.File;
 import java.io.PrintWriter;
+import java.util.Scanner;
+
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.DecompositionSolver;
@@ -12,118 +15,162 @@ import edu.cmu.sphinx.linguist.acoustic.tiedstate.Sphinx3Loader;
 
 public class Transform {
 
-	private float[][][][] As;
-	private float[][][] Bs;
-	private Sphinx3Loader loader;
-	private int nrOfClusters;
+    private float[][][][] As;
+    private float[][][] Bs;
+    private Sphinx3Loader loader;
+    private int nrOfClusters;
 
-	public Transform(Sphinx3Loader loader, int nrOfClusters) {
-		this.loader = loader;
-		this.nrOfClusters = nrOfClusters;
-	}
+    public Transform(Sphinx3Loader loader, int nrOfClusters) {
+        this.loader = loader;
+        this.nrOfClusters = nrOfClusters;
+    }
 
-	/**
-	 * Used for access to A matrix.
-	 * 
-	 * @return A matrix (representing A from A*x + B = C)
-	 */
-	public float[][][][] getAs() {
-		return As;
-	}
+    /**
+     * Used for access to A matrix.
+     * 
+     * @return A matrix (representing A from A*x + B = C)
+     */
+    public float[][][][] getAs() {
+        return As;
+    }
 
-	/**
-	 * Used for access to B matrix.
-	 * 
-	 * @return B matrix (representing B from A*x + B = C)
-	 */
-	public float[][][] getBs()  {
-		return Bs;
-	}
+    /**
+     * Used for access to B matrix.
+     * 
+     * @return B matrix (representing B from A*x + B = C)
+     */
+    public float[][][] getBs() {
+        return Bs;
+    }
 
-	/**
-	 * Writes the transformation to file in a format that could further be used
-	 * in Sphinx3 and Sphinx4.
-	 * 
-	 */
-	public void store(String filePath, int index) throws Exception {
-		PrintWriter writer = new PrintWriter(filePath, "UTF-8");
+    /**
+     * Writes the transformation to file in a format that could further be used
+     * in Sphinx3 and Sphinx4.
+     * 
+     */
+    public void store(String filePath, int index) throws Exception {
+        PrintWriter writer = new PrintWriter(filePath, "UTF-8");
 
-		// nMllrClass
-		writer.println("1");
-		writer.println(loader.getNumStreams());
+        // nMllrClass
+        writer.println("1");
+        writer.println(loader.getNumStreams());
 
-		for (int i = 0; i < loader.getNumStreams(); i++) {
-			writer.println(loader.getVectorLength()[i]);
+        for (int i = 0; i < loader.getNumStreams(); i++) {
+            writer.println(loader.getVectorLength()[i]);
 
-			for (int j = 0; j < loader.getVectorLength()[i]; j++) {
-				for (int k = 0; k < loader.getVectorLength()[i]; ++k) {
-					writer.print(As[index][i][j][k]);
-					writer.print(" ");
-				}
-				writer.println();
-			}
+            for (int j = 0; j < loader.getVectorLength()[i]; j++) {
+                for (int k = 0; k < loader.getVectorLength()[i]; ++k) {
+                    writer.print(As[index][i][j][k]);
+                    writer.print(" ");
+                }
+                writer.println();
+            }
 
-			for (int j = 0; j < loader.getVectorLength()[i]; j++) {
-				writer.print(Bs[index][i][j]);
-				writer.print(" ");
+            for (int j = 0; j < loader.getVectorLength()[i]; j++) {
+                writer.print(Bs[index][i][j]);
+                writer.print(" ");
 
-			}
-			writer.println();
+            }
+            writer.println();
 
-			for (int j = 0; j < loader.getVectorLength()[i]; j++) {
-				writer.print("1.0 ");
+            for (int j = 0; j < loader.getVectorLength()[i]; j++) {
+                writer.print("1.0 ");
 
-			}
-			writer.println();
-		}
-		writer.close();
-	}
+            }
+            writer.println();
+        }
+        writer.close();
+    }
 
-	/**
-	 * Used for computing the actual transformations (A and B matrices). These
-	 * are stored in As and Bs.
-	 */
-	private void computeMllrTransforms(double[][][][][] regLs,
-			double[][][][] regRs) {
-		int len;
-		DecompositionSolver solver;
-		RealMatrix coef;
-		RealVector vect, ABloc;
+    /**
+     * Used for computing the actual transformations (A and B matrices). These
+     * are stored in As and Bs.
+     */
+    private void computeMllrTransforms(double[][][][][] regLs,
+            double[][][][] regRs) {
+        int len;
+        DecompositionSolver solver;
+        RealMatrix coef;
+        RealVector vect, ABloc;
 
-		for (int c = 0; c < nrOfClusters; c++) {
-			this.As[c] = new float[loader.getNumStreams()][][];
-			this.Bs[c] = new float[loader.getNumStreams()][];
+        for (int c = 0; c < nrOfClusters; c++) {
+            this.As[c] = new float[loader.getNumStreams()][][];
+            this.Bs[c] = new float[loader.getNumStreams()][];
 
-			for (int i = 0; i < loader.getNumStreams(); i++) {
-				len = loader.getVectorLength()[i];
-				this.As[c][i] = new float[len][len];
-				this.Bs[c][i] = new float[len];
+            for (int i = 0; i < loader.getNumStreams(); i++) {
+                len = loader.getVectorLength()[i];
+                this.As[c][i] = new float[len][len];
+                this.Bs[c][i] = new float[len];
 
-				for (int j = 0; j < len; ++j) {
-					coef = new Array2DRowRealMatrix(regLs[c][i][j], false);
-					solver = new LUDecomposition(coef).getSolver();
-					vect = new ArrayRealVector(regRs[c][i][j], false);
-					ABloc = solver.solve(vect);
+                for (int j = 0; j < len; ++j) {
+                    coef = new Array2DRowRealMatrix(regLs[c][i][j], false);
+                    solver = new LUDecomposition(coef).getSolver();
+                    vect = new ArrayRealVector(regRs[c][i][j], false);
+                    ABloc = solver.solve(vect);
 
-					for (int k = 0; k < len; ++k) {
-						this.As[c][i][j][k] = (float) ABloc.getEntry(k);
-					}
+                    for (int k = 0; k < len; ++k) {
+                        this.As[c][i][j][k] = (float) ABloc.getEntry(k);
+                    }
 
-					this.Bs[c][i][j] = (float) ABloc.getEntry(len);
-				}
-			}
-		}
-	}
+                    this.Bs[c][i][j] = (float) ABloc.getEntry(len);
+                }
+            }
+        }
+    }
 
-	/**
-	 * Stores in current object a transform generated on the provided stats.
-	 * 
-	 * @param stats provided stats that were previously collected from Result objects.
-	 */
-	public void update(Stats stats) {
-		stats.fillRegLowerPart();
-		As = new float[nrOfClusters][][][];
-		Bs = new float[nrOfClusters][][];
-		this.computeMllrTransforms(stats.getRegLs(), stats.getRegRs());
-	}
+    /**
+     * Read the transformation from a file
+     * 
+     * @param filePath
+     * @throws FileNotFoundException
+     */
+    public void load(String filePath) throws Exception {
+
+        Scanner input = new Scanner(new File(filePath));
+        int numStreams, nMllrClass;
+        int[] vectorLength = new int[1];
+
+        nMllrClass = input.nextInt();
+        
+        assert nMllrClass == 1;
+        
+        numStreams = input.nextInt();
+
+        this.As = new float[nMllrClass][][][];
+        this.Bs = new float[nMllrClass][][];
+
+        for (int i = 0; i < numStreams; i++) {
+            vectorLength[i] = input.nextInt();
+
+            int length = vectorLength[i];
+
+            this.As[0] = new float[numStreams][length][length];
+            this.Bs[0] = new float[numStreams][length];
+
+            for (int j = 0; j < length; j++) {
+                for (int k = 0; k < length; ++k) {
+                    As[0][i][j][k] = input.nextFloat();
+                }
+            }
+
+            for (int j = 0; j < length; j++) {
+                Bs[0][i][j] = input.nextFloat();
+            }
+        }
+        input.close();
+    }
+
+    /**
+     * Stores in current object a transform generated on the provided stats.
+     * 
+     * @param stats
+     *            provided stats that were previously collected from Result
+     *            objects.
+     */
+    public void update(Stats stats) {
+        stats.fillRegLowerPart();
+        As = new float[nrOfClusters][][][];
+        Bs = new float[nrOfClusters][][];
+        this.computeMllrTransforms(stats.getRegLs(), stats.getRegRs());
+    }
 }
--- sphinx4-samples/src/main/java/edu/cmu/sphinx/demo/speakerid/SpeakerIdentificationDemo.java
+++ sphinx4-samples/src/main/java/edu/cmu/sphinx/demo/speakerid/SpeakerIdentificationDemo.java
@@ -4,19 +4,27 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+import edu.cmu.sphinx.decoder.adaptation.Stats;
+import edu.cmu.sphinx.decoder.adaptation.Transform;
 import edu.cmu.sphinx.speakerid.Segment;
 import edu.cmu.sphinx.speakerid.SpeakerCluster;
 import edu.cmu.sphinx.speakerid.SpeakerIdentification;
+import edu.cmu.sphinx.util.TimeFrame;
 
 public class SpeakerIdentificationDemo {
 
     /**
-     * Returns string version of the given time in miliseconds 
+     * Returns string version of the given time in miliseconds
+     * 
      * @param seconds
      * @return time in format mm:ss
      */
     public static String time(int seconds) {
-        return (seconds / 60000) + ":" + (Math.round((double) (seconds % 60000) / 1000));
+        return (seconds / 60000) + ":"
+                + (Math.round((double) (seconds % 60000) / 1000));
     }
 
     /**
@@ -26,22 +34,91 @@ public class SpeakerIdentificationDemo {
      *            speakers intervals
      * @throws IOException
      */
-    public static void printSpeakerIntervals(ArrayList<SpeakerCluster> speakers, String fileName)
+    public static void printSpeakerIntervals(
+            ArrayList<SpeakerCluster> speakers, String fileName)
             throws IOException {
         int idx = 0;
         for (SpeakerCluster spk : speakers) {
             idx++;
             ArrayList<Segment> segments = spk.getSpeakerIntervals();
             for (Segment seg : segments)
-                System.out.println(fileName + " " + " " + time(seg.getStartTime()) + " "
+                System.out.println(fileName + " " + " "
+                        + time(seg.getStartTime()) + " "
                         + time(seg.getLength()) + " Speaker" + idx);
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    /**
+     * @param speakers
+     *            An array of clusters for which it is needed to get the
+     *            speakers intervals for decoding with per-speaker adaptation
+     *            with diarization.
+     * @param url
+     *            Url for the audio
+     * @throws Exception
+     */
+    public static void speakerAdaptiveDecoding(ArrayList<SpeakerCluster> speakers,
+            URL url) throws Exception {
+
+        Configuration configuration = new Configuration();
+
+        // Load model from the jar
+        configuration
+                .setAcousticModelPath("resource:/edu/cmu/sphinx/models/acoustic/wsj");
+        configuration
+                .setDictionaryPath("resource:/edu/cmu/sphinx/models/acoustic/wsj/dict/cmudict.0.6d");
+        configuration
+                .setLanguageModelPath("resource:/edu/cmu/sphinx/models/language/en-us.lm.dmp");
+
+        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(
+                configuration);
+
+        TimeFrame t;
+        SpeechResult result;
+
+        for (SpeakerCluster spk : speakers) {
+            Stats stats = recognizer.createStats(1);
+            ArrayList<Segment> segments = spk.getSpeakerIntervals();
+
+            for (Segment s : segments) {
+                long startTime = s.getStartTime();
+                long endTime = s.getStartTime() + s.getLength();
+                t = new TimeFrame(startTime, endTime);
+
+                recognizer.startRecognition(url.openStream(), t);
+                while ((result = recognizer.getResult()) != null) {
+                    stats.collect(result);
+                }
+                recognizer.stopRecognition();
+            }
+
+            Transform profile;
+            // Create the Transformation
+            profile = stats.createTransform();
+            recognizer.setTransform(profile);
+
+            for (Segment seg : segments) {
+                long startTime = seg.getStartTime();
+                long endTime = seg.getStartTime() + seg.getLength();
+                t = new TimeFrame(startTime, endTime);
+
+                // Decode again with updated SpeakerProfile
+                recognizer.startRecognition(url.openStream(), t);
+                while ((result = recognizer.getResult()) != null) {
+                    System.out.format("Hypothesis: %s\n",
+                            result.getHypothesis());
+                }
+                recognizer.stopRecognition();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         SpeakerIdentification sd = new SpeakerIdentification();
         URL url = SpeakerIdentificationDemo.class.getResource("test.wav");
         ArrayList<SpeakerCluster> clusters = sd.cluster(url.openStream());
+
         printSpeakerIntervals(clusters, url.getPath());
+        speakerAdaptiveDecoding(clusters, url);
     }
 }
