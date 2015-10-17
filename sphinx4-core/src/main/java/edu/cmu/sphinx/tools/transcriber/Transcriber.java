5589021eb1b6aaed12fc5f8e62159280641739cb
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2015-10-17 19:37:07 +0000

    Added transcriber tool
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@13111 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/tools/transcriber/Transcriber.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 1999-2013 Carnegie Mellon University.
+ * Portions Copyright 2004 Sun Microsystems, Inc.
+ * Portions Copyright 2004 Mitsubishi Electric Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ */
+
+package edu.cmu.sphinx.tools.transcriber;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+
+public class Transcriber {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration();
+
+        configuration
+                .setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration
+                .setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration
+                .setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(
+                configuration);
+        InputStream stream = new FileInputStream(new File(args[0]));
+        stream.skip(44);
+
+        recognizer.startRecognition(stream);
+        SpeechResult result;
+        while ((result = recognizer.getResult()) != null) {
+            System.out.println(result.getHypothesis());
+        }
+        recognizer.stopRecognition();
+    }
+}
