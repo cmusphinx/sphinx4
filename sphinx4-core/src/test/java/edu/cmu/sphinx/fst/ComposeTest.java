bf700c8b5172d48caa518641cf59abfbc9b59633
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2015-12-05 16:12:06 +0000

    One more gradle update
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinx4@13128 94700074-3cef-4d97-a70e-9c8c206c02f5

--- sphinx4-core/src/test/java/edu/cmu/sphinx/fst/ComposeTest.java
+++ sphinx4-core/src/test/java/edu/cmu/sphinx/fst/ComposeTest.java
@@ -26,7 +26,7 @@ import edu.cmu.sphinx.fst.semiring.TropicalSemiring;
 /**
  * Compose Testing for Examples provided by M. Mohri,
  * "Weighted Automata Algorithms", Handbook of Weighted Automata,
- * Springer-Verlag, 2009, pp. 213–254.
+ * Springer-Verlag, 2009, pp. 213-254.
  * 
  * @author John Salatas
  */
