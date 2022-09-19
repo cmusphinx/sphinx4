025d40ab804a5bed814ce36a41416016ce7d86a8
sunrise <575563079@qq.com>
2022-09-19 16:04:02 +0800

    update to work with jdk11

--- sphinx4-core/src/main/java/edu/cmu/sphinx/fst/sequitur/SequiturImport.java
+++ sphinx4-core/src/main/java/edu/cmu/sphinx/fst/sequitur/SequiturImport.java
@@ -21,13 +21,13 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlMixed;
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlMixed;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 import edu.cmu.sphinx.fst.Fst;
 import edu.cmu.sphinx.fst.semiring.Semiring;
